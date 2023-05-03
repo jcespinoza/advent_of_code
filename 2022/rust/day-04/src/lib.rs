1ef44f4480fe46e373408f7c2507cc71de4e0193
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-05-03 16:54:35 -0600

    WIP: Adding 2022 Day 4 in Rust

---
+++ 2022/rust/day-04/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn process_part1(input: &str) -> u32 {
+  0
+}
+
+pub fn process_part2(input: &str) -> u32 {
+  0
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+    const INPUT: &str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+  #[test]
+  fn part1_works() {
+    let result = process_part1(INPUT);
+    assert_eq!(result, 2);
+  }
+
+  // #[test]
+  // fn part2_works() {
+  //   let result = process_part2(INPUT);
+  //   assert_eq!(result, 45000);
+  // }
+}
