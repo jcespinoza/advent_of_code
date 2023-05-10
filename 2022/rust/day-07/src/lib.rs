cd1ae3f3a96dc163e803493eb6fceaab7aec1b1e
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-05-09 21:40:22 -0600

    WIP Parsing the commands and ouput

--- 2022/rust/day-07/src/lib.rs
+++ 2022/rust/day-07/src/lib.rs
@@ -1,9 +1,82 @@
+use nom::{character::complete::{alpha1, newline}, character::{complete}, bytes::complete::{tag, take_until1}, IResult, branch::alt, sequence::separated_pair, multi::separated_list1};
+
 pub fn process_part1(input: &str) -> u32 {
+  let cmds = commands_parser(input).unwrap();
+  dbg!(&cmds);
+
   0
 }
 
 pub fn process_part2(input: &str) -> u32 {
   0
+}
+
+fn commands_parser(input: &str) -> IResult<&str,Vec<Command>> {
+  let (input, cmds) = separated_list1(newline, alt(
+    (ls_parser, cd_parser)
+  ))(input)?;
+  
+  Ok((input, cmds))
+}
+
+fn cd_parser(input: &str) -> IResult<&str,Command> {
+  dbg!("cd", input);
+  let (input, _) = tag("$ cd ")(input)?;
+  let (input, dir_name) = alt((tag(".."), alpha1, tag("/") ))(input)?;
+  let cmd = match dir_name {
+    "/" => Command::Cd(Cd::Root),
+    ".." => Command::Cd(Cd::Up),
+    name => Command::Cd(Cd::Into(name))
+  };
+  Ok((input, cmd))
+}
+
+fn ls_parser(input: &str) -> IResult<&str,Command> {
+  dbg!("ls", input);
+  let (input, _) = tag("$ ls")(input)?;
+  let (input, _) = newline(input)?;
+  let (input, files) = separated_list1(newline, alt((file_parser, dir_parser)))(input)?;
+  Ok((input, Command::Ls(files)))
+}
+
+fn file_parser(input: &str) -> IResult<&str,LsResult> {
+  let (input, (size, name)) = separated_pair(
+    nom::character::complete::u32,
+     tag(" "),
+     filename_parser
+  )(input)?;
+  Ok((input, LsResult::File{ size, name }))
+}
+
+fn filename_parser(input: &str) -> IResult<&str,&str> {
+  let (input, name) = take_until1("\n")(input)?;
+  Ok((input, name))
+}
+
+fn dir_parser(input: &str) -> IResult<&str,LsResult> {
+  let (input, _) = tag("dir ")(input)?;
+  let (input, name) = alpha1(input)?;
+  Ok((input, LsResult::Dir(name)))
+}
+
+
+#[derive(Debug)]
+enum Command<'a> {
+  Cd(Cd<'a>),
+  Ls(Vec<LsResult<'a>>),
+}
+
+#[derive(Debug)]
+enum Cd<'a>{
+  Up,
+  Into(&'a str),
+  Root,
+}
+
+#[derive(Debug)]
+enum LsResult<'a> {
+  File {size: u32, name: &'a str},
+  Dir(&'a str)
 }
 
 #[cfg(test)]
