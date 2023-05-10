984643f53963e3c2660fb59b754956c340bfe35d
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-05-10 10:57:33 -0600

    WIP: Skipping last line of input if it's a file :(

--- 2022/rust/day-07/src/lib.rs
+++ 2022/rust/day-07/src/lib.rs
@@ -1,87 +1,161 @@
-use nom::{character::complete::{alpha1, newline}, character::{complete}, bytes::complete::{tag, take_until1}, IResult, branch::alt, sequence::separated_pair, multi::separated_list1};
+use std::collections::BTreeMap;
+
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_until1},
+    character::complete::{alpha1, newline},
+    multi::separated_list1,
+    sequence::separated_pair,
+    IResult,
+};
 
 pub fn process_part1(input: &str) -> u32 {
-  let cmds = commands_parser(input).unwrap();
-  dbg!(&cmds);
+    let (input, cmds) = commands_parser(input).unwrap();
+dbg!(input);
+    let filesystem = build_filesystem(cmds);
 
-  0
+    let dir_sizes = filesystem.dir_sizes();
+
+    0
+}
+
+fn build_filesystem(cmds: Vec<Command>) -> FileSystem {
+    let mut directories: BTreeMap<String, Vec<File>> = BTreeMap::new();
+
+    let mut route: Vec<&str> = vec![];
+
+    for cmd in cmds.iter() {
+        match cmd {
+            Command::Cd(Cd::Root) => {
+                route.push("");
+            }
+            Command::Cd(Cd::Up) => {
+                route.pop();
+            }
+            Command::Cd(Cd::Into(dir)) => {
+                route.push(dir);
+            }
+            Command::Ls(files) => {
+                directories.entry(route.join("/")).or_insert(vec![]);
+
+                for f in files.iter() {
+                    match f {
+                        LsResult::File { size, name } => {
+                            directories.entry(route.join("/")).and_modify(|v| {
+                                v.push(File {
+                                    size: *size,
+                                    name: name,
+                                });
+                            });
+                        }
+                        LsResult::Dir(_) => (),
+                    }
+                }
+            }
+        }
+    }
+    FileSystem { directories }
+}
+
+#[derive(Debug)]
+struct FileSystem<'a> {
+    directories: BTreeMap<String, Vec<File<'a>>>,
+}
+
+impl FileSystem<'_> {
+    fn dir_sizes(&self) -> BTreeMap<String, u32> {
+        let mut sizes: BTreeMap<String, u32> = BTreeMap::new();
+
+        for (path, files) in self.directories.iter() {
+            let child_dirs = path.split("/").collect::<Vec<&str>>();
+            let current_size = files.iter().map(|f| f.size).sum::<u32>();
+
+            for index in 0..child_dirs.len() {
+                sizes
+                    .entry(child_dirs[0..=index].join("/"))
+                    .and_modify(|v| *v += current_size)
+                    .or_insert(current_size);
+            }
+        }
+        sizes
+    }
 }
 
 pub fn process_part2(input: &str) -> u32 {
-  0
+    0
 }
 
-fn commands_parser(input: &str) -> IResult<&str,Vec<Command>> {
-  let (input, cmds) = separated_list1(newline, alt(
-    (ls_parser, cd_parser)
-  ))(input)?;
-  
-  Ok((input, cmds))
+fn commands_parser(input: &str) -> IResult<&str, Vec<Command>> {
+    let (input, cmds) = separated_list1(newline, alt((ls_parser, cd_parser)))(input)?;
+
+    Ok((input, cmds))
 }
 
-fn cd_parser(input: &str) -> IResult<&str,Command> {
-  dbg!("cd", input);
-  let (input, _) = tag("$ cd ")(input)?;
-  let (input, dir_name) = alt((tag(".."), alpha1, tag("/") ))(input)?;
-  let cmd = match dir_name {
-    "/" => Command::Cd(Cd::Root),
-    ".." => Command::Cd(Cd::Up),
-    name => Command::Cd(Cd::Into(name))
-  };
-  Ok((input, cmd))
+fn cd_parser(input: &str) -> IResult<&str, Command> {
+    let (input, _) = tag("$ cd ")(input)?;
+    let (input, dir_name) = alt((tag(".."), alpha1, tag("/")))(input)?;
+    let cmd = match dir_name {
+        "/" => Command::Cd(Cd::Root),
+        ".." => Command::Cd(Cd::Up),
+        name => Command::Cd(Cd::Into(name)),
+    };
+    Ok((input, cmd))
 }
 
-fn ls_parser(input: &str) -> IResult<&str,Command> {
-  dbg!("ls", input);
-  let (input, _) = tag("$ ls")(input)?;
-  let (input, _) = newline(input)?;
-  let (input, files) = separated_list1(newline, alt((file_parser, dir_parser)))(input)?;
-  Ok((input, Command::Ls(files)))
+fn ls_parser(input: &str) -> IResult<&str, Command> {
+    let (input, _) = tag("$ ls")(input)?;
+    let (input, _) = newline(input)?;
+    let (input, files) = separated_list1(newline, alt((file_parser, dir_parser)))(input)?;
+    // dbg!(input, &files);
+    Ok((input, Command::Ls(files)))
 }
 
-fn file_parser(input: &str) -> IResult<&str,LsResult> {
-  let (input, (size, name)) = separated_pair(
-    nom::character::complete::u32,
-     tag(" "),
-     filename_parser
-  )(input)?;
-  Ok((input, LsResult::File{ size, name }))
+fn file_parser(input: &str) -> IResult<&str, LsResult> {
+    let (input, (size, name)) =
+        separated_pair(nom::character::complete::u32, tag(" "), filename_parser)(input)?;
+    Ok((input, LsResult::File { size, name }))
 }
 
-fn filename_parser(input: &str) -> IResult<&str,&str> {
-  let (input, name) = take_until1("\n")(input)?;
-  Ok((input, name))
+fn filename_parser(input: &str) -> IResult<&str, &str> {
+    let (input, name) = take_until1("\n")(input)?;
+
+    Ok((input, name))
 }
 
-fn dir_parser(input: &str) -> IResult<&str,LsResult> {
-  let (input, _) = tag("dir ")(input)?;
-  let (input, name) = alpha1(input)?;
-  Ok((input, LsResult::Dir(name)))
+fn dir_parser(input: &str) -> IResult<&str, LsResult> {
+    let (input, _) = tag("dir ")(input)?;
+    let (input, name) = alpha1(input)?;
+    Ok((input, LsResult::Dir(name)))
 }
 
+#[derive(Debug)]
+struct File<'a> {
+    name: &'a str,
+    size: u32,
+}
 
 #[derive(Debug)]
 enum Command<'a> {
-  Cd(Cd<'a>),
-  Ls(Vec<LsResult<'a>>),
+    Cd(Cd<'a>),
+    Ls(Vec<LsResult<'a>>),
 }
 
 #[derive(Debug)]
-enum Cd<'a>{
-  Up,
-  Into(&'a str),
-  Root,
+enum Cd<'a> {
+    Up,
+    Into(&'a str),
+    Root,
 }
 
 #[derive(Debug)]
 enum LsResult<'a> {
-  File {size: u32, name: &'a str},
-  Dir(&'a str)
+    File { size: u32, name: &'a str },
+    Dir(&'a str),
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
     const INPUT: &str = "$ cd /
 $ ls
 dir a
@@ -106,16 +180,16 @@ $ ls
 5626152 d.ext
 7214296 k";
 
-  #[test]
-  fn part1_works() {
-    let result = process_part1(INPUT);
-    assert_eq!(result, 95437);
-  }
+    #[test]
+    fn part1_works() {
+        let result = process_part1(INPUT);
+        assert_eq!(result, 95437);
+    }
 
-  #[test]
-  #[ignore]
-  fn part2_works() {
-    let result = process_part2(INPUT);
-    assert_eq!(result, 45000);
-  }
+    #[test]
+    #[ignore]
+    fn part2_works() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, 45000);
+    }
 }
