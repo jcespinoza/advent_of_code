6858945cbe7fad8fbd06b2d2be6a38b163eff22f
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-05-10 12:29:06 -0600

    WIP: Parsing grid into data structure

--- 2022/rust/day-08/src/lib.rs
+++ 2022/rust/day-08/src/lib.rs
@@ -1,4 +1,10 @@
 pub fn process_part1(input: &str) -> u32 {
+  let grid_of_trees:Vec<Vec<u32>> = input.lines().map(|line| {
+    line.chars().map(|c| c.to_digit(10).unwrap()).collect()
+  }).collect();
+
+  dbg!(&grid_of_trees);
+
   0
 }
 
@@ -9,12 +15,16 @@ pub fn process_part2(input: &str) -> u32 {
 #[cfg(test)]
 mod tests {
   use super::*;
-    const INPUT: &str = "SOME";
+    const INPUT: &str = "30373
+25512
+65332
+33549
+35390";
 
   #[test]
   fn part1_works() {
     let result = process_part1(INPUT);
-    assert_eq!(result, 24000);
+    assert_eq!(result, 21);
   }
 
   #[test]
