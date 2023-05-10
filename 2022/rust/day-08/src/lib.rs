8c960e199ec7fb26f3b5fa000c81cdedcf72b49d
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-05-10 11:58:35 -0600

    WIP: Adding starter files for rust day 08

---
+++ 2022/rust/day-08/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn process_part1(input: &str) -> u32 {
+  0
+}
+
+pub fn process_part2(input: &str) -> u32 {
+  0
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+    const INPUT: &str = "SOME";
+
+  #[test]
+  fn part1_works() {
+    let result = process_part1(INPUT);
+    assert_eq!(result, 24000);
+  }
+
+  #[test]
+  #[ignore]
+  fn part2_works() {
+    let result = process_part2(INPUT);
+    assert_eq!(result, 45000);
+  }
+}
