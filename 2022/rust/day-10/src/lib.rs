ab6bdc9630ee66602b98c08e0d6c6b12f4a4f252
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-05-21 14:43:01 -0600

    WIP Day 10 Rust Part 1 working

--- 2022/rust/day-10/src/lib.rs
+++ 2022/rust/day-10/src/lib.rs
@@ -1,47 +1,64 @@
-use nom::{sequence::separated_pair, branch::alt, IResult, bytes::complete::tag, character::complete::{self, newline}, multi::separated_list1};
+use std::collections::BTreeMap;
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{self, newline},
+    multi::separated_list1,
+    sequence::separated_pair,
+    IResult,
+};
 
 pub fn process_part1(input: &str) -> u32 {
     let (_, instructions) = parse_instructions(input).unwrap();
-    
-    
-  0
+
+    let x_values = get_register_x_at_cycle(instructions);
+
+    println!("{:?}", x_values);
+    x_values.iter().sum::<i32>() as u32
 }
 
 pub fn process_part2(input: &str) -> u32 {
-  0
+    0
 }
 
-pub fn get_signal_during_cycle(instructions: &[Instruction], cycle: u32) -> i32 {
-  if cycle <= 1 { return 1; }
+pub fn get_register_x_at_cycle(instructions: Vec<Instruction>) -> Vec<i32> {
+    let mut value_of_register_x = 1;
+    let mut x_values: BTreeMap<u32, i32> = BTreeMap::new();
 
-  let instructions_subset = instructions.iter().take((cycle-1) as usize).collect::<Vec<&Instruction>>();
-  let last_instruction = instructions_subset.last().unwrap();
+    let mut current_cycle = 1;
 
-  let is_last_instruction_addx = match **last_instruction { 
-    Instruction::Addx(_) => true,
-    _ => false,
-  };
-  
-  let addx_values = instructions_subset.iter().filter_map(|f| match f {
-    Instruction::Noop => None,
-    Instruction::Addx(value) => Some(value),
-  });
+    for instruction in &instructions {
+        if ((current_cycle + 1) as i32 - 20) % 40 == 0 {
+            x_values.insert(
+                current_cycle + 1,
+                value_of_register_x * ((current_cycle + 1) as i32),
+            );
+        }
 
-  
-  let mut signal_strength = 1 + addx_values.sum::<i32>();
+        current_cycle += instruction.duration();
 
-  let instructions_taken = instructions_subset.len() as u32;
-  let are_there_enough_instructions =  (cycle as i32 - instructions_taken as i32).abs() > 1;
-  if are_there_enough_instructions && is_last_instruction_addx {
-    signal_strength -= match last_instruction { Instruction::Addx(v) => v, _ => &0 }; 
-  }
+        match instruction {
+            Instruction::Noop => {}
+            Instruction::Addx(value) => {
+                value_of_register_x += value;
+            }
+        }
 
-  signal_strength
+        if (current_cycle as i32 - 20) % 40 == 0 {
+            x_values.insert(
+                current_cycle as u32,
+                value_of_register_x * (current_cycle as i32),
+            );
+        }
+    }
+
+    x_values.values().map(|v| *v).collect()
 }
 
 fn parse_instructions(input: &str) -> IResult<&str, Vec<Instruction>> {
-  let (input, instructions) = separated_list1(newline, alt((parse_noop, parse_addx)))(input)?;
-  Ok((input, instructions))
+    let (input, instructions) = separated_list1(newline, alt((parse_noop, parse_addx)))(input)?;
+    Ok((input, instructions))
 }
 
 fn parse_addx(input: &str) -> IResult<&str, Instruction> {
@@ -56,13 +73,22 @@ fn parse_noop(input: &str) -> IResult<&str, Instruction> {
 
 #[derive(Debug)]
 pub enum Instruction {
-  Noop,
-  Addx(i32),
+    Noop,
+    Addx(i32),
+}
+
+impl Instruction {
+    fn duration(&self) -> u32 {
+        match self {
+            Instruction::Noop => 1,
+            Instruction::Addx(_) => 2,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
     const INPUT_1: &str = "noop
 addx 3
 addx -5";
@@ -214,73 +240,17 @@ noop
 noop
 noop";
 
-  #[test]
-  fn x_starts_during_1(){
-    let (_,instructions) = parse_instructions(INPUT_1).unwrap();
-    let signal = get_signal_during_cycle(&instructions, 1);
-    assert_eq!(signal, 1);
-  }
+    #[test]
+    // #[ignore]
+    fn part1_2_works() {
+        let result = process_part1(INPUT_2);
+        assert_eq!(result, 13140);
+    }
 
-  #[test]
-  fn x_does_not_change_during_cycle_2(){
-    let (_,instructions) = parse_instructions(INPUT_1).unwrap();
-    let signal = get_signal_during_cycle(&instructions, 2);
-    assert_eq!(signal, 1);
-  }
-
-  #[test]
-  fn x_does_not_change_during_cycle_3(){
-    let (_,instructions) = parse_instructions(INPUT_1).unwrap();
-    let signal = get_signal_during_cycle(&instructions, 3);
-    assert_eq!(signal, 1);
-  }
-  
-  #[test]
-  fn x_changes_after_cycle_4(){
-    let (_,instructions) = parse_instructions(INPUT_1).unwrap();
-    let signal = get_signal_during_cycle(&instructions, 4);
-    assert_eq!(signal, 4);
-  }
-  
-  #[test]
-  fn x_does_not_change_during_cycle_5(){
-    let (_,instructions) = parse_instructions(INPUT_1).unwrap();
-    let signal = get_signal_during_cycle(&instructions, 5);
-    assert_eq!(signal, 4);
-  }
-  
-  #[test]
-  fn x_changes_after_cycle_5(){
-    let (_,instructions) = parse_instructions(INPUT_1).unwrap();
-    let signal = get_signal_during_cycle(&instructions, 6);
-    assert_eq!(signal, -1);
-  }
-
-  #[test]
-  #[ignore]
-  fn part1_1_works() {
-    let result = process_part1(INPUT_1);
-    assert_eq!(result, 24000);
-  }
-
-  #[test]
-  #[ignore]  
-  fn part1_2_works() {
-    let result = process_part1(INPUT_2);
-    assert_eq!(result, 13140);
-  }
-
-  #[test]
-  #[ignore]
-  fn part2_1_works() {
-    let result = process_part2(INPUT_1);
-    assert_eq!(result, 45000);
-  }
-
-  #[test]
-  #[ignore]
-  fn part2_2_works() {
-    let result = process_part2(INPUT_2);
-    assert_eq!(result, 45000);
-  }
+    #[test]
+    #[ignore]
+    fn part2_2_works() {
+        let result = process_part2(INPUT_2);
+        assert_eq!(result, 45000);
+    }
 }
