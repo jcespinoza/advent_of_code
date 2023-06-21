c836337d48ce09d81e3bb0cbb989ce73edfe2f83
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-06-20 18:23:26 -0600

    WIP Day 12 Rust - Parsing grid of heights

--- 2022/rust/day-12/src/lib.rs
+++ 2022/rust/day-12/src/lib.rs
@@ -1,30 +1,82 @@
+use std::collections::HashMap;
+
 pub fn process_part1(input: &str) -> u32 {
-  0
+    parse_heightmap(input);
+
+    0
 }
 
 pub fn process_part2(input: &str) -> u32 {
-  0
+    todo!()
+}
+
+fn parse_heightmap(input: &str) -> Input {
+    let lines = input.lines();
+    let mut start_coord = (0, 0);
+    let mut end_coord = (0, 0);
+
+    let heightmap: HashMap<Coord, u32> = lines
+        .enumerate()
+        .flat_map(|(line_index, line_item)| {
+            line_item
+                .chars()
+                .enumerate()
+                .map(move |(col_index, char_item)| {
+                    let height_character = match char_item {
+                        'S' => {
+                            start_coord = (col_index as u32, line_index as u32);
+                            // S has height a
+                            'a'
+                        }
+                        'E' => {
+                            end_coord = (col_index as u32, line_index as u32);
+                            // E has height z
+                            'z'
+                        }
+                        _ => char_item,
+                    };
+                    let height_value = height_character as u32 - 'a' as u32;
+                    ((col_index as u32, line_index as u32), height_value)
+                })
+        })
+        .collect();
+
+    let input = Input {
+        start: start_coord,
+        end: end_coord,
+        heightmap,
+    };
+
+    todo!()
+}
+
+type Coord = (u32, u32);
+
+struct Input {
+    start: Coord,
+    end: Coord,
+    heightmap: HashMap<Coord, u32>,
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
     const INPUT: &str = "Sabqponm
 abcryxxl
 accszExk
 acctuvwj
 abdefghi";
 
-  #[test]
-  fn part1_works() {
-    let result = process_part1(INPUT);
-    assert_eq!(result, 31);
-  }
+    #[test]
+    fn part1_works() {
+        let result = process_part1(INPUT);
+        assert_eq!(result, 31);
+    }
 
-  #[test]
-  #[ignore]
-  fn part2_works() {
-    let result = process_part2(INPUT);
-    assert_eq!(result, 45000);
-  }
+    #[test]
+    #[ignore]
+    fn part2_works() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, 45000);
+    }
 }
