32154df47174013dc747825db8065ce1a3ec1a8a
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-12-17 00:35:31 -0600

    Adding 2023 Day 11 in C#

---
+++ 2023/csharp/Advent23.Days/Day11/Day11Solver.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Commons;
+
+namespace Advent23.Days
+{
+    public class Day11Solver : Solver<object[], long>
+    {
+        public Day11Solver() : base(2023, 11) { }
+
+        public override object[] ParseInput(IEnumerable<string> input)
+            => input.ToArray();
+
+
+        public override long PartOne(object[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long PartTwo(object[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
