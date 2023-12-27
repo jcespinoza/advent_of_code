361f890f88812a960746b9449f99760e66ec10b1
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-12-27 15:46:24 -0600

    Completed Part One

---
+++ 2023/csharp/Advent23.Days/DayTemplate - Copy/Day00Solver.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode.Commons;
+
+namespace Advent23.Days
+{
+    public class Day15Solver : Solver<string, long>
+    {
+        public Day15Solver() : base(2023, 15) { }
+
+        public override string ParseInput(IEnumerable<string> input)
+            => input.First();
+
+
+        public override long PartOne(string input)
+        {
+            var stepStrs = input.Trim().Split(',', StringSplitOptions.TrimEntries);
+            var currentHash = 0;
+            var totalStepSum = 0;
+            for (int stepIdx = 0; stepIdx < stepStrs.Length; stepIdx++)
+            {
+                var newHash = HashSequence(stepStrs[stepIdx], currentHash);
+                totalStepSum += newHash;
+            }
+
+            return totalStepSum;
+        }
+
+        private int HashSequence(string sequence, int currentHash)
+        {
+            for (int index = 0; index < sequence.Length; index++)
+            {
+                var cChar = sequence[index];
+                currentHash += cChar;
+                currentHash *= 17;
+                currentHash %= 256;
+            }
+
+            return currentHash;
+        }
+
+        public override long PartTwo(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 2023/csharp/Advent23.Days/DayTemplate - Copy/Day00Test.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode.Commons;
+using dotenv.net;
+
+namespace Advent23.Days
+{
+    public class Day15Test : TestEngine<Day15Solver, string, long>
+    {
+        private const long EXPECTED_SOLUTION_PART_1 = 513_214;
+        private const long EXPECTED_SOLUTION_PART_2 = 123;
+
+        public Day15Test()
+        {
+            DotEnv.Load();
+        }
+
+        public override Puzzle PartOne => new()
+        {
+            ShouldSkipTests = false,
+            Example = new()
+            {
+                RawInput = [
+                    "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+                ],
+                Result = 1320,
+            },
+            Examples = [
+                new()
+                {
+                    RawInput = [
+                        "HASH"
+                    ],
+                    Result = 52,
+                },
+            ],
+            Solution = EXPECTED_SOLUTION_PART_1,
+        };
+
+        public override Puzzle PartTwo => new()
+        {
+            ShouldSkipTests = true,
+            Example = new()
+            {
+                RawInput = [
+                ],
+                Result = 8,
+            },
+            Solution = EXPECTED_SOLUTION_PART_2,
+        };
+    }
+}
