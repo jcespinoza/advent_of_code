452feb02f63b44e165e460a6ccc5c2583e778e3d
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-12-27 17:16:07 -0600

    Adding 2023 Day 16 in C#

---
+++ 2023/csharp/Advent23.Days/Day16/Day16Test.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode.Commons;
+using dotenv.net;
+
+namespace Advent23.Days
+{
+    public class Day16Test : TestEngine<Day16Solver, object[], long>
+    {
+        private const long EXPECTED_SOLUTION_PART_1 = 123;
+        private const long EXPECTED_SOLUTION_PART_2 = 123;
+
+        public Day16Test()
+        {
+            DotEnv.Load();
+        }
+
+        public override Puzzle PartOne => new()
+        {
+            ShouldSkipTests = false,
+            Example = new()
+            {
+                RawInput = [
+                    @".|...\....",
+                    @"|.-.\.....",
+                    @".....|-...",
+                    @"........|.",
+                    @"..........",
+                    @".........\",
+                    @"..../.\\..",
+                    @".-.-/..|..",
+                    @".|....-|.\",
+                    @"..//.|....",
+                ],
+                Result = 46,
+            },
+            Examples = [
+            ],
+            Solution = EXPECTED_SOLUTION_PART_1,
+        };
+
+        public override Puzzle PartTwo => new()
+        {
+            ShouldSkipTests = true,
+            Example = new()
+            {
+                RawInput = [
+                ],
+                Result = 8,
+            },
+            Solution = EXPECTED_SOLUTION_PART_2,
+        };
+    }
+}
