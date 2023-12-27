36bd6414dace5a9e0b2c88c8d63244dfb8e7e8f6
Juan Carlos Espinoza <dev@jcespinoza.com>
2023-12-26 20:26:42 -0600

    Addin 2023 Day 13 in C# (#23)
    * Adding 2023 Day 13 in C#
    
    * Completed Part One
    
    Refused to accept part two as a single-solution problem for now

---
+++ 2023/csharp/Advent23.Days/Day13/Day13Solver.cs
@@ -0,0 +1,38 @@
+﻿using Advent23.Days.Day13;
+using AdventOfCode.Commons;
+
+namespace Advent23.Days
+{
+    public class Day13Solver : Solver<TerrainPattern[], long>
+    {
+        public Day13Solver() : base(2023, 13) { }
+
+        public override TerrainPattern[] ParseInput(IEnumerable<string> input)
+            => TerrainPattern.ParsePatternGroups(input);
+
+        public override long PartOne(TerrainPattern[] patterns)
+        {
+            long totalSummary = 0;
+            foreach (var pattern in patterns)
+            {
+                pattern.HMirrorIndexes = TerrainPattern.ComputeHMirrorIndexes(pattern);
+
+                pattern.VMirrorIndexes = TerrainPattern.ComputeVMirrorIndexes(pattern);
+
+                int hReflectedLines = TerrainPattern.ComputeHReflectedLines(pattern);
+                int vReflectedLines = TerrainPattern.ComputeVReflectedLines(pattern);
+
+                int hCount = 100 * hReflectedLines;
+                int patternSummary = vReflectedLines + hCount;
+
+                totalSummary += patternSummary;
+            }
+            return totalSummary;
+        }
+
+        public override long PartTwo(TerrainPattern[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
