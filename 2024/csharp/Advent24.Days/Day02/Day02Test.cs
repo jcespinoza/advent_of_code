65cc9d1184473897c3cb144071ae37cf6836d888
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-02 07:04:30 -0600

    Adding 2024 Day 02 in C#

---
+++ 2024/csharp/Advent24.Days/Day02/Day02Test.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode.Commons;
+using dotenv.net;
+
+namespace Advent24.Days
+{
+    public class Day02Test : TestEngine<Day02Solver, object[], long>
+    {
+        private const long EXPECTED_SOLUTION_PART_1 = 123;
+        private const long EXPECTED_SOLUTION_PART_2 = 123;
+
+        public Day02Test()
+        {
+            DotEnv.Load();
+        }
+
+        public override Puzzle PartOne => new()
+        {
+            ShouldSkipTests = true,
+            Example = new()
+            {
+                RawInput = [
+                ],
+                Result = 8,
+            },
+            Examples = [
+            ],
+            Solution = EXPECTED_SOLUTION_PART_1,
+        };
+
+        public override Puzzle PartTwo => new()
+        {
+            ShouldSkipTests = true,
+            Example = new()
+            {
+                RawInput = [
+                ],
+                Result = 8,
+            },
+            Solution = EXPECTED_SOLUTION_PART_2,
+        };
+    }
+}
