29a567f433f6dee1667d80632d52163f6e32411a
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-04 07:55:57 -0600

    Completed part 2

--- 2024/csharp/Advent24.Days/Day02/Day02Solver.cs
+++ 2024/csharp/Advent24.Days/Day02/Day02Solver.cs
@@ -24,9 +24,44 @@ namespace Advent24.Days
             return safeReports.Count(safe => safe);
         }
 
-        public override long PartTwo(Report[] input)
+        public override long PartTwo(Report[] reports)
         {
-            throw new NotImplementedException();
+            List<Report> safeReports = [];
+            List<Report> unsafeReports = [];
+            foreach (var item in reports)
+            {
+                if(item.IsSafe())
+                {
+                    safeReports.Add(item);
+                }
+                else
+                {
+                    unsafeReports.Add(item);
+                }
+            }
+
+            List<Report> problemDampened = CheckWithProblemDampening(unsafeReports).ToList();
+
+            return safeReports.Count + problemDampened.Count;
+        }
+
+        private static IEnumerable<Report> CheckWithProblemDampening(List<Report> unsafeReports)
+        {
+            foreach (var item in unsafeReports)
+            {
+                // try removing levels and check again
+                for (int index = 0; index < item.Levels.Length; index++)
+                {
+                    var newLevels = item.Levels.ToList();
+                    newLevels.RemoveAt(index);
+                    var newReport = new Report { Levels = [.. newLevels] };
+                    if (newReport.IsSafe())
+                    {
+                        yield return newReport;
+                        break; // make sure we stop after first success
+                    }
+                }
+            }
         }
     }
 }
--- 2024/csharp/Advent24.Days/Day02/Day02Test.cs
+++ 2024/csharp/Advent24.Days/Day02/Day02Test.cs
@@ -9,7 +9,7 @@ namespace Advent24.Days
     public class Day02Test : TestEngine<Day02Solver, Report[], long>
     {
         private const long EXPECTED_SOLUTION_PART_1 = 631;
-        private const long EXPECTED_SOLUTION_PART_2 = 123;
+        private const long EXPECTED_SOLUTION_PART_2 = 665;
 
         public Day02Test()
         {
@@ -38,12 +38,18 @@ namespace Advent24.Days
 
         public override Puzzle PartTwo => new()
         {
-            ShouldSkipTests = true,
+            ShouldSkipTests = false,
             Example = new()
             {
                 RawInput = [
+                    "7 6 4 2 1",
+                    "1 2 7 8 9",
+                    "9 7 6 2 1",
+                    "1 3 2 4 5",
+                    "8 6 4 4 1",
+                    "1 3 6 7 9",
                 ],
-                Result = 8,
+                Result = 4,
             },
             Solution = EXPECTED_SOLUTION_PART_2,
         };
