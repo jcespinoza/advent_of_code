a793259e3ccaabcaf3a3feb317b4fdbc0bd0fa6d
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-07 11:24:10 -0600

    Completed parser function

--- 2024/csharp/Advent24.Days/Day05/Day05Solver.cs
+++ 2024/csharp/Advent24.Days/Day05/Day05Solver.cs
@@ -1,23 +1,70 @@
 ﻿using AdventOfCode.Commons;
+using System.Net;
+using Xunit;
 
 namespace Advent24.Days
 {
-    public class Day05Solver : Solver<object[], long>
+    public class Day05Solver : Solver<PrintingConfig, long>
     {
         public Day05Solver() : base(2024, 05) { }
 
-        public override object[] ParseInput(IEnumerable<string> input)
-            => input.ToArray();
+        public override PrintingConfig ParseInput(IEnumerable<string> input)
+        {
+            int index = 0;
+            var lineArray = input.ToArray();
+            var currentLine = string.Empty;
+            List<OrderingRule> rules = [];
+            do
+            {
+                currentLine = lineArray[index];
+
+                var parts = currentLine.Split('|');
+                Assert.Equal(2, parts.Length);
+
+                var pageX = int.Parse(parts[0]);
+                var pageY = int.Parse(parts[1]);
+
+                rules.Add(new OrderingRule(pageX, pageY));
+
+                index++;
+            } while (!string.IsNullOrWhiteSpace(currentLine));
+
+            // A blank line was found. Skip to the next line
+            index++;
+
+            List<int[]> updates = [];
+            while(index < lineArray.Length)
+            {
+                currentLine = lineArray[index];
+
+                Assert.False(string.IsNullOrWhiteSpace(currentLine));
+
+                var pages = currentLine.Split(',').ToList();
+                updates.Add(pages.Select(int.Parse).ToArray());
+
+                index++;
+            }
+
+            return new PrintingConfig { Rules = rules, Updates = updates };
+        }
 
 
-        public override long PartOne(object[] input)
+        public override long PartOne(PrintingConfig input)
         {
             throw new NotImplementedException();
         }
 
-        public override long PartTwo(object[] input)
+        public override long PartTwo(PrintingConfig input)
         {
             throw new NotImplementedException();
         }
+    }
+
+    public record OrderingRule(int PageX, int PageY);
+
+    public class PrintingConfig
+    {
+        public List<OrderingRule> Rules { get; init; } = [];
+        public List<int[]> Updates { get; init; } = [];
     }
 }
--- 2024/csharp/Advent24.Days/Day05/Day05Test.cs
+++ 2024/csharp/Advent24.Days/Day05/Day05Test.cs
@@ -3,7 +3,7 @@ using dotenv.net;
 
 namespace Advent24.Days
 {
-    public class Day05Test : TestEngine<Day05Solver, object[], long>
+    public class Day05Test : TestEngine<Day05Solver, PrintingConfig, long>
     {
         private const long EXPECTED_SOLUTION_PART_1 = 123;
         private const long EXPECTED_SOLUTION_PART_2 = 123;
@@ -15,12 +15,40 @@ namespace Advent24.Days
 
         public override Puzzle PartOne => new()
         {
-            ShouldSkipTests = true,
+            ShouldSkipTests = false,
             Example = new()
             {
                 RawInput = [
+                    "47|53",
+                    "97|13",
+                    "97|61",
+                    "97|47",
+                    "75|29",
+                    "61|13",
+                    "75|53",
+                    "29|13",
+                    "97|29",
+                    "53|29",
+                    "61|53",
+                    "97|53",
+                    "61|29",
+                    "47|13",
+                    "75|47",
+                    "97|75",
+                    "47|61",
+                    "75|61",
+                    "47|29",
+                    "75|13",
+                    "53|13",
+                    "",
+                    "75,47,61,53,29",
+                    "97,61,53,29,13",
+                    "75,29,13",
+                    "75,97,47,61,53",
+                    "61,13,29",
+                    "97,13,75,29,47",
                 ],
-                Result = 8,
+                Result = 143,
             },
             Examples = [
             ],
