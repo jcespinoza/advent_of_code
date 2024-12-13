19456c2e10974c6aaf1fc10dc519dca3758f7c06
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-13 06:59:18 -0600

    Starting disk mapping logic

--- 2024/csharp/Advent24.Days/Day09/Day09Solver.cs
+++ 2024/csharp/Advent24.Days/Day09/Day09Solver.cs
@@ -10,9 +10,34 @@ namespace Advent24.Days
             => input.First().Select(c => c - '0').ToArray();
 
 
-        public override long PartOne(int[] input)
+        public override long PartOne(int[] diskMap)
+        {
+            List<DiskBlock> blocks = BuildBlocks(diskMap);
+            int[] sortedDisk = SortBlocks(blocks);
+            long checksum = GetFileSytemChecksum(sortedDisk);
+
+            return checksum;
+        }
+
+        private List<DiskBlock> BuildBlocks(int[] diskMap)
         {
             throw new NotImplementedException();
+        }
+
+        private int[] SortBlocks(List<DiskBlock> blocks)
+        {
+            throw new NotImplementedException();
+        }
+
+        private long GetFileSytemChecksum(int[] sortedDisk)
+        {
+            long totalSum = 0;
+            for (int index = 0; index < sortedDisk.Length; index++)
+            {
+                long localSum = index * sortedDisk[index];
+                totalSum += localSum;
+            }
+            return totalSum;
         }
 
         public override long PartTwo(int[] input)
@@ -20,4 +45,6 @@ namespace Advent24.Days
             throw new NotImplementedException();
         }
     }
+
+    public record DiskBlock(int Size, int Index);
 }
--- 2024/csharp/Advent24.Days/Day09/Day09Test.cs
+++ 2024/csharp/Advent24.Days/Day09/Day09Test.cs
@@ -1,9 +1,11 @@
 ﻿using AdventOfCode.Commons;
 using dotenv.net;
+using FluentAssertions;
+using Xunit;
 
 namespace Advent24.Days
 {
-    public class Day09Test : TestEngine<Day09Solver, object[], long>
+    public class Day09Test : TestEngine<Day09Solver, int[], long>
     {
         private const long EXPECTED_SOLUTION_PART_1 = 123;
         private const long EXPECTED_SOLUTION_PART_2 = 123;
@@ -39,5 +41,20 @@ namespace Advent24.Days
             },
             Solution = EXPECTED_SOLUTION_PART_2,
         };
+
+        [Fact]
+        public void PartOne_Example_01()
+        {
+            // Arrange
+            Day09Solver day09Solver = new Day09Solver();
+            var input = PartOne.Example.Input ?? day09Solver.ParseInput(["20202"]);
+            var expectation = 0*1+6*1+9*2;
+            // represents 001122
+
+            // Act
+            var result = day09Solver.PartOne(input);
+            // Assert
+            result.Should().Be(expectation);
+        }
     }
 }
