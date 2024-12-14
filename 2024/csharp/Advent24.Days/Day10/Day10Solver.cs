45cf8125c76a85c8de39d5e2faeeccf0e8800728
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-14 13:19:23 -0600

    Parsing done

--- 2024/csharp/Advent24.Days/Day10/Day10Solver.cs
+++ 2024/csharp/Advent24.Days/Day10/Day10Solver.cs
@@ -2,20 +2,23 @@
 
 namespace Advent24.Days
 {
-    public class Day10Solver : Solver<object[], long>
+    public class Day10Solver : Solver<int[][], long>
     {
         public Day10Solver() : base(2024, 10) { }
 
-        public override object[] ParseInput(IEnumerable<string> input)
-            => input.ToArray();
+        public override int[][] ParseInput(IEnumerable<string> input)
+            => input.Select(
+                l => l.Select(c => c == '.' ? -1 : c - '0')
+                      .ToArray()
+            ).ToArray();
 
 
-        public override long PartOne(object[] input)
+        public override long PartOne(int[][] input)
         {
             throw new NotImplementedException();
         }
 
-        public override long PartTwo(object[] input)
+        public override long PartTwo(int[][] input)
         {
             throw new NotImplementedException();
         }
