aec7a8ada23baf7313a7ca93284276b109b7ceb3
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-14 14:51:15 -0600

    Adding 2024 Day 10 in C# (#37)
    * Adding 2024 Day 10 in C#
    
    * Setting up tests
    
    * Parsing done
    
    * Completed Part One by traversing using a queue
    
    * Setting up tests for Part Two
    
    * Completed Part Two by recursively exploring all paths

---
+++ 2024/csharp/Advent24.Days/Day10/Day10Solver.cs
@@ -0,0 +1,141 @@
+﻿using AdventOfCode.Commons;
+using FluentAssertions.Equivalency.Tracing;
+
+namespace Advent24.Days
+{
+    public class Day10Solver : Solver<int[][], long>
+    {
+        public Day10Solver() : base(2024, 10) { }
+
+        public override int[][] ParseInput(IEnumerable<string> input)
+            => input.Select(
+                l => l.Select(c => c == '.' ? -1 : c - '0')
+                      .ToArray()
+            ).ToArray();
+
+
+        public override long PartOne(int[][] topoMap)
+        {
+            List<(int, int)> trailheads = GetTrailHeads(topoMap);
+
+            long totalScore = 0;
+            foreach (var head in trailheads)
+            {
+                (int row, int col) = head;
+                int score = GetHikingScore(topoMap, row, col);
+                totalScore += score;
+            }
+
+            return totalScore;
+        }
+
+        private static List<(int, int)> GetTrailHeads(int[][] topoMap)
+        {
+            List<(int, int)> heads = [];
+            for (int row = 0; row < topoMap.Length; row++)
+            {
+                for (int col = 0; col < topoMap[row].Length; col++)
+                {
+                    if(topoMap[row][col] == 0)
+                    {
+                        heads.Add((row, col));
+                    }
+                }
+            }
+
+            return heads;
+        }
+
+        private static int GetHikingScore(int[][] topoMap, int startRow, int startCol)
+        {
+            HashSet<(int, int)> visited = [];
+            Queue<(int, int)> toVisit = new();
+            toVisit.Enqueue((startRow, startCol));
+            HashSet<(int, int)> uniqueTops = [];
+
+            while(toVisit.Count > 0)
+            {
+                (int row, int col) = toVisit.Dequeue();
+                visited.Add((row, col));
+                if (topoMap[row][col] == 9)
+                {
+                    uniqueTops.Add((row, col));
+                    continue;
+                }
+
+                ProcessAdjacentCell(topoMap, visited, toVisit, row, col, Direction.North);
+                ProcessAdjacentCell(topoMap, visited, toVisit, row, col, Direction.East);
+                ProcessAdjacentCell(topoMap, visited, toVisit, row, col, Direction.South);
+                ProcessAdjacentCell(topoMap, visited, toVisit, row, col, Direction.West);
+            }
+
+            return uniqueTops.Count;
+        }
+
+        private static void ProcessAdjacentCell(int[][] topoMap, HashSet<(int, int)> visited, Queue<(int, int)> toVisit, int row, int col, Direction direction)
+        {
+            Result<(int, int), string> targetCell = GridWalker<int>.Move(topoMap, direction, row, col);
+            if (targetCell.IsFailure) return;
+
+            (int nRow, int nCol) = targetCell.Value;
+
+            bool alreadyVisited = visited.Contains(targetCell.Value);
+            bool isGradualAscension = topoMap[nRow][nCol] - topoMap[row][col] == 1;
+            if (!alreadyVisited && isGradualAscension)
+            {
+                toVisit.Enqueue(targetCell.Value);
+            }
+        }
+
+        public override long PartTwo(int[][] topoMap)
+        {
+            List<(int, int)> trailheads = GetTrailHeads(topoMap);
+
+            long totalRating = 0;
+            foreach (var head in trailheads)
+            {
+                (int row, int col) = head;
+                long rating = GetHikingRating(topoMap, row, col);
+                totalRating += rating;
+            }
+
+            return totalRating;
+        }
+
+        private int GetHikingRating(int[][] topoMap, int row, int col)
+        {
+            int paths = GetNumberOfPathsToTop(topoMap, row, col);
+
+            return paths;
+        }
+
+        private int GetNumberOfPathsToTop(int[][] topoMap, int row, int col)
+        {
+            int height = topoMap[row][col];
+            if (height >= 9) return 0;
+
+            int paths = 0;
+            Direction[] cardinalPoints = [Direction.North, Direction.East, Direction.South, Direction.West];
+            foreach (var direction in cardinalPoints)
+            {
+                Result<(int,int), string> targetCell = GridWalker<int>.Move(topoMap, direction, row, col);
+                if(targetCell.IsFailure) continue;
+
+                (int nRow, int nCol) = targetCell.Value;
+                int targetHeight = topoMap[nRow][nCol];
+
+                if (targetHeight - height != 1) continue;
+                if(targetHeight == 9)
+                {
+                    paths++;
+                    continue;
+                }
+                
+                int pathsInDirection = GetNumberOfPathsToTop(topoMap, nRow, nCol);
+                paths += pathsInDirection;
+            }
+
+            return paths;
+        }
+    }
+}
---
+++ 2024/csharp/Advent24.Days/Day10/Day10Test.cs
@@ -0,0 +1,145 @@
+﻿using AdventOfCode.Commons;
+using dotenv.net;
+
+namespace Advent24.Days
+{
+    public class Day10Test : TestEngine<Day10Solver, int[][], long>
+    {
+        private const long EXPECTED_SOLUTION_PART_1 = 517;
+        private const long EXPECTED_SOLUTION_PART_2 = 123;
+
+        public Day10Test()
+        {
+            DotEnv.Load();
+        }
+
+        public override Puzzle PartOne => new()
+        {
+            ShouldSkipTests = false,
+            Example = new()
+            {
+                RawInput = [
+                        "89010123",
+                        "78121874",
+                        "87430965",
+                        "96549874",
+                        "45678903",
+                        "32019012",
+                        "01329801",
+                        "10456732",
+                    ],
+                Result = 36,
+            },
+            Examples = [
+                new()
+                {
+                    RawInput = [
+                        "0123",
+                        "1234",
+                        "8765",
+                        "9876",
+                    ],
+                    Result = 1,
+                },
+                new()
+                {
+                    RawInput = [
+                        "...0...",
+                        "...1...",
+                        "...2...",
+                        "6543456",
+                        "7.....7",
+                        "8.....8",
+                        "9.....9",
+                    ],
+                    Result = 2,
+                },
+                new()
+                {
+                    RawInput = [
+                        "..90..9",
+                        "...1.98",
+                        "...2..7",
+                        "6543456",
+                        "765.987",
+                        "876....",
+                        "987....",
+                    ],
+                    Result = 4,
+                },
+                new()
+                {
+                    RawInput = [
+                        "10..9..",
+                        "2...8..",
+                        "3...7..",
+                        "4567654",
+                        "...8..3",
+                        "...9..2",
+                        ".....01",
+                    ],
+                    Result = 3,
+                },
+            ],
+            Solution = EXPECTED_SOLUTION_PART_1,
+        };
+
+        public override Puzzle PartTwo => new()
+        {
+            ShouldSkipTests = false,
+            Example = new()
+            {
+                RawInput = [
+                    "89010123",
+                    "78121874",
+                    "87430965",
+                    "96549874",
+                    "45678903",
+                    "32019012",
+                    "01329801",
+                    "10456732",
+                ],
+                Result = 81,
+            },
+
+            Examples = [
+                new(){
+                    RawInput = [
+                        ".....0.",
+                        "..4321.",
+                        "..5..2.",
+                        "..6543.",
+                        "..7..4.",
+                        "..8765.",
+                        "..9....",
+                    ],
+                    Result = 3,
+                },
+                new(){
+                    RawInput = [
+                        "..90..9",
+                        "...1.98",
+                        "...2..7",
+                        "6543456",
+                        "765.987",
+                        "876....",
+                        "987....",
+                    ],
+                    Result = 13,
+                },
+                new(){
+                    RawInput = [
+                        "012345",
+                        "123456",
+                        "234567",
+                        "345678",
+                        "4.6789",
+                        "56789.",
+                    ],
+                    Result = 227,
+                },
+            ],
+            Solution = EXPECTED_SOLUTION_PART_2,
+        };
+    }
+}
