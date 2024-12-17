5d1d23fa7e74e7acadc486aa4f3b66400abc0f17
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-17 11:46:11 -0600

    Unit test failing for actual solution

--- 2024/csharp/Advent24.Days/Day13/Day13Solver.cs
+++ 2024/csharp/Advent24.Days/Day13/Day13Solver.cs
@@ -38,15 +38,62 @@ namespace Advent24.Days
 
                 MachineConfig machineConfig = new(prizeX, prizeY, buttonA, buttonB);
                 machineConfigs.Add(machineConfig);
+                index += 2;
             }
 
-            return machineConfigs.ToArray();
+            return [.. machineConfigs];
         }
 
 
-        public override long PartOne(MachineConfig[] input)
+        public override long PartOne(MachineConfig[] configs)
         {
-            throw new NotImplementedException();
+            const int A_PRICE = 3;
+            const int B_PRICE = 1;
+
+            /* Upon analysis; the solution is to solve the system of equations and use that to determine the first press-count that will get to the given coordinates
+             The system of equations is as follows:
+             AxS + BxT = Px
+             AyS + ByT = Py
+                where:
+                    Ax, Ay are the x and y offsets of button A
+                    Bx, By are the x and y offsets of button B
+                    Px, Py are the x and y coordinates of the prize
+                    S, T are the number of times button A and B are pressed respectively
+
+                We can solve this system of equations by multiplying the first equation by By and the second by Bx and subtracting the two equations to get:
+                (AxBy - AyBx)S = (PxBy - PyBx)
+                S = (PxBy - PyBx) / (AxBy - AyBx)
+
+                When computed we can use the value of S in the equation for T:
+                T = (Px - AxS) / Bx
+
+                We can then use the first equation to determine the number of presses needed to get to the prize coordinates
+             */
+
+            long tokensUsed = 0;
+            foreach (var config in configs)
+            {
+                var Ax = config.A.XOffset;
+                var Ay = config.A.YOffset;
+                var Bx = config.B.XOffset;
+                var By = config.B.YOffset;
+                var Px = config.PrizeX;
+                var Py = config.PrizeY;
+                var S = (Px * By - Py * Bx) / (Ax * By - Ay * Bx);
+                var T = (Px - Ax * S) / Bx;
+
+                // Discard solutions that are not integers
+                if (S % 1 != 0 || T % 1 != 0) continue;
+                // Discard solutions that represent more than 100 button presses
+                if (S > 100 || T > 100) continue;
+
+                // Now mutiple the butto presses by the price of each
+                var costA = S * A_PRICE;
+                var costB = T * B_PRICE;
+                tokensUsed += costA + costB;
+            }
+
+            return tokensUsed;
         }
 
         public override long PartTwo(MachineConfig[] input)
