6052361fcda39a5f744b42385fff7280f54c2347
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-17 11:59:17 -0600

    Adding 2024 Day 13 in C# (#41)
    * Adding 2024 Day 13 in C#
    
    * Parsing completed
    
    * Unit test failing for actual solution
    
    * Completed Part One and Part Two by using systems of equations

---
+++ 2024/csharp/Advent24.Days/Day13/Day13Solver.cs
@@ -0,0 +1,113 @@
+﻿using AdventOfCode.Commons;
+
+namespace Advent24.Days
+{
+    public class Day13Solver : Solver<MachineConfig[], long>
+    {
+        public Day13Solver() : base(2024, 13) { }
+
+        public override MachineConfig[] ParseInput(IEnumerable<string> input)
+        {
+            List<MachineConfig> machineConfigs = [];
+            var lines = input.ToArray();
+            for (int index = 0; index < lines.Length; index++)
+            {
+                var line = lines[index];
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                if (!line.StartsWith("Button A")) break;
+
+                var buttonAconfig = line.Split(":")[1].Trim();
+                var buttonBconfig = lines[index + 1].Split(":")[1].Trim();
+                var prizeConfig = lines[index + 2].Split(":")[1].Trim();
+
+                var buttonAtext = buttonAconfig.Split(", ");
+                var buttonBtext = buttonBconfig.Split(", ");
+                var prizeText = prizeConfig.Split(", ");
+
+                var buttonAX = int.Parse(buttonAtext[0].Split("+")[1]);
+                var buttonAY = int.Parse(buttonAtext[1].Split("+")[1]);
+                MachineButton buttonA = new(buttonAX, buttonAY);
+
+                var buttonBX = int.Parse(buttonBtext[0].Split("+")[1]);
+                var buttonBY = int.Parse(buttonBtext[1].Split("+")[1]);
+                MachineButton buttonB = new(buttonBX, buttonBY);
+
+                var prizeX = int.Parse(prizeText[0].Split("=")[1]);
+                var prizeY = int.Parse(prizeText[1].Split("=")[1]);
+
+                MachineConfig machineConfig = new(prizeX, prizeY, buttonA, buttonB);
+                machineConfigs.Add(machineConfig);
+                index += 2;
+            }
+
+            return [.. machineConfigs];
+        }
+
+
+        public override long PartOne(MachineConfig[] configs)
+        {
+            return ComputeTokenCost(configs, withConversionFix: false);
+        }
+
+        private static long ComputeTokenCost(MachineConfig[] configs, bool withConversionFix)
+        {
+            const int A_PRICE = 3;
+            const int B_PRICE = 1;
+
+            /* Upon analysis; the solution is to solve the system of equations and use that to determine the first press-count that will get to the given coordinates
+             The system of equations is as follows:
+             AxS + BxT = Px
+             AyS + ByT = Py
+                where:
+                    Ax, Ay are the x and y offsets of button A
+                    Bx, By are the x and y offsets of button B
+                    Px, Py are the x and y coordinates of the prize
+                    S, T are the number of times button A and B are pressed respectively
+
+                We can solve this system of equations by multiplying the first equation by By and the second by Bx and subtracting the two equations to get:
+                (AxBy - AyBx)S = (PxBy - PyBx)
+                S = (PxBy - PyBx) / (AxBy - AyBx)
+
+                When computed we can use the value of S in the equation for T:
+                T = (Px - AxS) / Bx
+
+                We can then use the first equation to determine the number of presses needed to get to the prize coordinates
+             */
+
+            long conversionFix = withConversionFix ? 10000000000000 : 0;
+            bool limitPresses = !withConversionFix;
+            long tokensUsed = 0;
+            foreach (var config in configs)
+            {
+                var Ax = config.A.XOffset;
+                var Ay = config.A.YOffset;
+                var Bx = config.B.XOffset;
+                var By = config.B.YOffset;
+                var Px = config.PrizeX + conversionFix;
+                var Py = config.PrizeY + conversionFix;
+
+                // Perform floating-point division
+                var S = (double)(Px * By - Py * Bx) / (Ax * By - Ay * Bx);
+                var T = (double)(Px - Ax * S) / Bx;
+
+                // Discard solutions that are not integers
+                if (S % 1 != 0 || T % 1 != 0) continue;
+                // Discard solutions that represent more than 100 button presses
+                if (limitPresses && (S > 100 || T > 100)) continue;
+
+                // Now multiply the button presses by the price of each
+                var costA = (long)(S * A_PRICE);
+                var costB = (long)(T * B_PRICE);
+                tokensUsed += costA + costB;
+            }
+
+            return tokensUsed;
+        }
+
+        public override long PartTwo(MachineConfig[] configs)
+        {
+            return ComputeTokenCost(configs, withConversionFix: true);
+        }
+    }
+}
---
+++ 2024/csharp/Advent24.Days/Day13/Day13Test.cs
@@ -0,0 +1,72 @@
+﻿using AdventOfCode.Commons;
+using dotenv.net;
+
+namespace Advent24.Days
+{
+    public class Day13Test : TestEngine<Day13Solver, MachineConfig[], long>
+    {
+        private const long EXPECTED_SOLUTION_PART_1 = 36250L;
+        private const long EXPECTED_SOLUTION_PART_2 = 83232379451012L;
+
+        public Day13Test()
+        {
+            DotEnv.Load();
+        }
+
+        public override Puzzle PartOne => new()
+        {
+            ShouldSkipTests = false,
+            Example = new()
+            {
+                RawInput = [
+                    "Button A: X+94, Y+34",
+                    "Button B: X+22, Y+67",
+                    "Prize: X=8400, Y=5400",
+                    "",
+                    "Button A: X+26, Y+66",
+                    "Button B: X+67, Y+21",
+                    "Prize: X=12748, Y=12176",
+                    "",
+                    "Button A: X+17, Y+86",
+                    "Button B: X+84, Y+37",
+                    "Prize: X=7870, Y=6450",
+                    "",
+                    "Button A: X+69, Y+23",
+                    "Button B: X+27, Y+71",
+                    "Prize: X=18641, Y=10279",
+                ],
+                Result = 480,
+            },
+            Examples = [
+            ],
+            Solution = EXPECTED_SOLUTION_PART_1,
+        };
+
+        public override Puzzle PartTwo => new()
+        {
+            ShouldSkipTests = false,
+            Example = new()
+            {
+                RawInput = [
+                    "Button A: X+94, Y+34",
+                    "Button B: X+22, Y+67",
+                    "Prize: X=8400, Y=5400",
+                    "",
+                    "Button A: X+26, Y+66",
+                    "Button B: X+67, Y+21",
+                    "Prize: X=12748, Y=12176",
+                    "",
+                    "Button A: X+17, Y+86",
+                    "Button B: X+84, Y+37",
+                    "Prize: X=7870, Y=6450",
+                    "",
+                    "Button A: X+69, Y+23",
+                    "Button B: X+27, Y+71",
+                    "Prize: X=18641, Y=10279",
+                ],
+                Result = 875318608908L,
+            },
+            Solution = EXPECTED_SOLUTION_PART_2,
+        };
+    }
+}
