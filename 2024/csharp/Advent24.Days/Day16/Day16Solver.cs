733ef842c6601138daabf38746ac02335b3976de
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-21 10:14:43 -0600

    Adding 2024 Day 16 in C#

---
+++ 2024/csharp/Advent24.Days/Day16/Day16Solver.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Commons;
+
+namespace Advent24.Days
+{
+    public class Day16Solver : Solver<object[], long>
+    {
+        public Day16Solver() : base(2024, 16) { }
+
+        public override object[] ParseInput(IEnumerable<string> input)
+            => input.ToArray();
+
+
+        public override long PartOne(object[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long PartTwo(object[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 2024/csharp/Advent24.Days/Day16/Day16Test.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode.Commons;
+using dotenv.net;
+
+namespace Advent24.Days
+{
+    public class Day16Test : TestEngine<Day16Solver, object[], long>
+    {
+        private const long EXPECTED_SOLUTION_PART_1 = 123;
+        private const long EXPECTED_SOLUTION_PART_2 = 123;
+
+        public Day16Test()
+        {
+            DotEnv.Load();
+        }
+
+        public override Puzzle PartOne => new()
+        {
+            ShouldSkipTests = true,
+            Example = new()
+            {
+                RawInput = [
+                ],
+                Result = 8,
+            },
+            Examples = [
+            ],
+            Solution = EXPECTED_SOLUTION_PART_1,
+        };
+
+        public override Puzzle PartTwo => new()
+        {
+            ShouldSkipTests = true,
+            Example = new()
+            {
+                RawInput = [
+                ],
+                Result = 8,
+            },
+            Solution = EXPECTED_SOLUTION_PART_2,
+        };
+    }
+}
