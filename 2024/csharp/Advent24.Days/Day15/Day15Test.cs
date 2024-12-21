9fd3cc86427386445459e0ee3e850a3663012b68
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-21 10:12:07 -0600

    Completed Part Two after realizing unit tests were wrong

--- 2024/csharp/Advent24.Days/Day15/Day15Test.cs
+++ 2024/csharp/Advent24.Days/Day15/Day15Test.cs
@@ -6,7 +6,7 @@ namespace Advent24.Days
     public class Day15Test : SteppedTestEngine<Day15Solver, Warehouse, Warehouse, long>
     {
         private const long EXPECTED_SOLUTION_PART_1 = 1412971L;
-        private const long EXPECTED_SOLUTION_PART_2 = 123;
+        private const long EXPECTED_SOLUTION_PART_2 = 1429299L;
 
         public Day15Test()
         {
@@ -70,6 +70,34 @@ namespace Advent24.Days
             Example = new()
             {
                 RawInput = [
+                    "##########",
+                    "#..O..O.O#",
+                    "#......O.#",
+                    "#.OO..O.O#",
+                    "#..O@..O.#",
+                    "#O#..O...#",
+                    "#O..O..O.#",
+                    "#.OO.O.OO#",
+                    "#....O...#",
+                    "##########",
+                    "",
+                    "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+                    "vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+                    "><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+                    "<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+                    "^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+                    "^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+                    ">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+                    "<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+                    "^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+                    "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+                ],
+                Result = 9021,
+            },
+            Examples = [
+                new()
+                {
+                    RawInput = [
                         "########",
                         "#..O.O.#",
                         "##@.O..#",
@@ -80,9 +108,10 @@ namespace Advent24.Days
                         "########",
                         "",
                         "<^^>>>vv<v>>v<<",
-                ],
-                Result = 9021,
-            },
+                    ],
+                    Result = 1751,
+                },
+            ],
             Solution = EXPECTED_SOLUTION_PART_2,
         };
     }
