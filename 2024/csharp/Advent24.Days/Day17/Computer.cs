713cb420bb083a89afab68650f10f685c242a052
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-22 16:24:30 -0600

    Small Refactorings

--- 2024/csharp/Advent24.Days/Day17/Computer.cs
+++ 2024/csharp/Advent24.Days/Day17/Computer.cs
@@ -1,4 +1,6 @@
-﻿namespace Advent24.Days
+﻿using System.Text;
+
+namespace Advent24.Days
 {
     public record Computer
     {
@@ -13,12 +15,53 @@
             BDV,
             CDV
         }
+
         public bool Ready { get; set; }
         public int ProgramPointer { get; set; }
         public int RegA { get; set; }
         public int RegB { get; set; }
         public int RegC { get; set; }
         public int[] Program { get; set; } = [];
+
+        public static void SimulateProgram(Computer computer, ref StringBuilder strBuilder)
+        {
+            long cycles = 0;
+            while (computer.Ready)
+            {
+                cycles++;
+                int opCode = computer.ReadNext();
+                Instruction instruction = (Instruction)opCode;
+                switch (instruction)
+                {
+                    case Instruction.ADV:
+                        ExecuteAdv(computer);
+                        break;
+                    case Instruction.BXL:
+                        ExecuteBxl(computer);
+                        break;
+                    case Instruction.BST:
+                        ExecuteBst(computer);
+                        break;
+                    case Instruction.JNZ:
+                        ExecuteJnz(computer);
+                        break;
+                    case Instruction.BXC:
+                        ExecuteBxc(computer);
+                        break;
+                    case Instruction.OUT:
+                        ExecuteOut(computer, strBuilder);
+                        break;
+                    case Instruction.BDV:
+                        ExecuteBdv(computer);
+                        break;
+                    case Instruction.CDV:
+                        ExecuteCdv(computer);
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+        }
 
         public void MovePointer(int offset)
         {
@@ -50,6 +93,110 @@
             int instruction = Program[ProgramPointer];
             MovePointer(1);
             return instruction;
+        }
+        
+        public static void ExecuteCdv(Computer computer)
+        {
+            PerformDv(computer, result => computer.RegC = result);
+        }
+
+        public static void ExecuteBdv(Computer computer)
+        {
+            PerformDv(computer, result => computer.RegB = result);
+        }
+
+        public static void ExecuteOut(Computer computer, StringBuilder strBuilder)
+        {
+            var operand = GetComboOperand(computer);
+            var modulo8 = operand % 8;
+            if (strBuilder.Length == 0)
+            {
+                strBuilder.Append(modulo8);
+            }
+            else
+            {
+                strBuilder.Append($",{modulo8}");
+            }
+        }
+
+        public static void ExecuteBxc(Computer computer)
+        {
+            // Read operand but ignore it
+            var operand = computer.ReadNext();
+            var xorResult = computer.RegB ^ computer.RegC;
+            computer.RegB = xorResult;
+        }
+
+        public static void ExecuteJnz(Computer computer)
+        {
+            if (computer.RegA == 0)
+            {
+                computer.MovePointer(2);
+            }
+            else
+            {
+                int operand = GetLiteral(computer, isForJump: true);
+                computer.JumpTo(operand);
+            }
+        }
+
+        public static void ExecuteBst(Computer computer)
+        {
+            var operand = GetComboOperand(computer);
+            var modulo8 = operand % 8;
+            computer.RegB = modulo8;
+        }
+
+        public static void ExecuteBxl(Computer computer)
+        {
+            var operand = GetLiteral(computer);
+            var xorResult = computer.RegB ^ operand;
+            computer.RegB = xorResult;
+        }
+
+        public static void ExecuteAdv(Computer computer)
+        {
+            PerformDv(computer, result => computer.RegA = result);
+        }
+
+        private static void PerformDv(Computer computer, Action<int> action)
+        {
+            var numerator = computer.RegA;
+            int actualOperand = GetComboOperand(computer);
+            var denominator = Math.Pow(2, actualOperand);
+
+            var result = (int)Math.Floor(numerator / denominator);
+            action(result);
+        }
+
+        private static int GetComboOperand(Computer computer)
+        {
+            var comboOperand = computer.ReadNext();
+            var actualOperand = ResolveComboOperand(computer, comboOperand);
+            return actualOperand;
+        }
+
+        public static int GetLiteral(Computer computer, bool isForJump = false)
+        {
+            var literal = computer.Program[computer.ProgramPointer];
+
+            if (!isForJump)
+            {
+                computer.MovePointer(1);
+            }
+
+            return literal;
+        }
+
+        private static int ResolveComboOperand(Computer computer, int comboOperand)
+        {
+            if (comboOperand > 0 && comboOperand <= 3) return comboOperand;
+
+            if (comboOperand == 4) return computer.RegA;
+            if (comboOperand == 5) return computer.RegB;
+            if (comboOperand == 6) return computer.RegC;
+
+            throw new Exception($"Combo Operand {comboOperand} is not valid!");
         }
     }
 }
---
+++ 2024/csharp/Advent24.Days/Day17/ComputerTests.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Advent24.Days.Day17
+{
+    public class ComputerTests
+    {
+        [Fact]
+        public void ProgramExample_01()
+        {
+            var computer = new Computer
+            {
+                RegC = 9,
+                Program = [2, 6],
+                Ready = true,
+            };
+            var strBuilder = new StringBuilder();
+            Computer.SimulateProgram(computer, ref strBuilder);
+
+            computer.RegB.Should().Be(1);
+        }
+
+        [Fact]
+        public void ProgramExample_02()
+        {
+            var computer = new Computer
+            {
+                RegA = 10,
+                Program = [5, 0, 5, 1, 5, 4],
+                Ready = true,
+            };
+            var strBuilder = new StringBuilder();
+            Computer.SimulateProgram(computer, ref strBuilder);
+            var output = strBuilder.ToString();
+
+            output.Should().Be("0,1,2");
+        }
+
+        [Fact]
+        public void ProgramExample_03()
+        {
+            var computer = new Computer
+            {
+                RegA = 2024,
+                Program = [0, 1, 5, 4, 3, 0],
+                Ready = true,
+            };
+            var strBuilder = new StringBuilder();
+            Computer.SimulateProgram(computer, ref strBuilder);
+            var output = strBuilder.ToString();
+
+            output.Should().Be("4,2,5,6,7,7,7,7,3,1,0");
+            computer.RegA.Should().Be(0);
+        }
+
+        [Fact]
+        public void ProgramExample_04()
+        {
+            var computer = new Computer
+            {
+                RegB = 29,
+                Program = [1, 7],
+                Ready = true,
+            };
+            var strBuilder = new StringBuilder();
+            Computer.SimulateProgram(computer, ref strBuilder);
+
+            computer.RegB.Should().Be(26);
+        }
+
+        [Fact]
+        public void ProgramExample_05()
+        {
+            var computer = new Computer
+            {
+                RegB = 2024,
+                RegC = 43690,
+                Program = [4, 0],
+                Ready = true,
+            };
+            var strBuilder = new StringBuilder();
+            Computer.SimulateProgram(computer, ref strBuilder);
+            
+            computer.RegB.Should().Be(44354);
+        }
+    }
+}
