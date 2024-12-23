d990232f54b871df96a4aab24b1e2e04dca39d3d
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-23 16:26:37 -0600

    Completed Part One

--- 2024/csharp/Advent24.Days/Day18/Day18Solver.cs
+++ 2024/csharp/Advent24.Days/Day18/Day18Solver.cs
@@ -2,20 +2,104 @@
 
 namespace Advent24.Days
 {
-    public class Day18Solver : Solver<object[], long>
+    public class Day18Solver : Solver<(int,int)[], long>
     {
         public Day18Solver() : base(2024, 18) { }
 
-        public override object[] ParseInput(IEnumerable<string> input)
-            => input.ToArray();
+        public override (int,int)[] ParseInput(IEnumerable<string> input)
+            => input
+            .Select(l => l.Split(','))
+            .Select(l =>
+                (
+                    int.Parse(l[0]),
+                    int.Parse(l[1])
+                )
+            )
+            .ToArray();
 
 
-        public override long PartOne(object[] input)
+        public override long PartOne((int,int)[] byteLocations)
         {
-            throw new NotImplementedException();
+            int gridSize = 70;
+            int bytesToSimulate = 1024;
+            if (byteLocations.Length < 500)
+            {
+                // This is an example and not the real puzzle
+                gridSize = 6;
+                bytesToSimulate = 12;
+            }
+
+            char[][] map = GetMap(gridSize + 1);
+            long stepsToExit = ComputeStepsToExit(map, byteLocations, bytesToSimulate);
+
+            return stepsToExit;
         }
 
-        public override long PartTwo(object[] input)
+        private static char[][] GetMap(int gridSize)
+        {
+            var map = new char[gridSize][];
+            for (int i = 0; i < gridSize; i++)
+            {
+                map[i] = new char[gridSize];
+            }
+            return map;
+        }
+
+        private static long ComputeStepsToExit(char[][] map, (int, int)[] byteLocations, int bytesToSimulate)
+        {
+            (int, int) exitLocation = (map.Length - 1, map.Length - 1);
+            for(int index = 0; index < bytesToSimulate; index++)
+            {
+                (int x, int y) = byteLocations[index];
+                map[y][x] = '#';
+            }
+
+            Queue<(int, int, int)> cellsToVisit = [];
+            cellsToVisit.Enqueue((0, 0, 0));
+            HashSet<(int, int)> visited = [];
+
+            while (cellsToVisit.Count > 0)
+            {
+                (int cRow, int cCol, int cSteps) = cellsToVisit.Dequeue();
+
+                foreach (var nLocation in new (int, int)[] {
+                    (cRow + 1, cCol),
+                    (cRow - 1, cCol),
+                    (cRow, cCol + 1),
+                    (cRow, cCol - 1)
+                })
+                {
+                    (int nRow, int nCol) = nLocation;
+                    if (nRow < 0 || nRow >= map.Length || nCol < 0 || nCol >= map[0].Length)
+                    {
+                        continue;
+                    }
+
+                    if (map[nRow][nCol] == '#')
+                    {
+                        continue;
+                    }
+
+                    if (visited.Contains((nRow, nCol)))
+                    {
+                        continue;
+                    }
+
+                    visited.Add((nRow, nCol));
+                    if ((nRow, nCol) == exitLocation)
+                    {
+                        return cSteps + 1;
+                    }
+
+                    visited.Add((nRow, nCol));
+                    cellsToVisit.Enqueue((nRow, nCol, cSteps + 1));
+                }
+            }
+
+            return long.MaxValue;
+        }
+
+        public override long PartTwo((int,int)[] input)
         {
             throw new NotImplementedException();
         }
--- 2024/csharp/Advent24.Days/Day18/Day18Test.cs
+++ 2024/csharp/Advent24.Days/Day18/Day18Test.cs
@@ -3,9 +3,9 @@ using dotenv.net;
 
 namespace Advent24.Days
 {
-    public class Day18Test : TestEngine<Day18Solver, object[], long>
+    public class Day18Test : TestEngine<Day18Solver, (int,int)[], long>
     {
-        private const long EXPECTED_SOLUTION_PART_1 = 123;
+        private const long EXPECTED_SOLUTION_PART_1 = 298;
         private const long EXPECTED_SOLUTION_PART_2 = 123;
 
         public Day18Test()
@@ -15,12 +15,37 @@ namespace Advent24.Days
 
         public override Puzzle PartOne => new()
         {
-            ShouldSkipTests = true,
+            ShouldSkipTests = false,
             Example = new()
             {
                 RawInput = [
+                    "5,4",
+                    "4,2",
+                    "4,5",
+                    "3,0",
+                    "2,1",
+                    "6,3",
+                    "2,4",
+                    "1,5",
+                    "0,6",
+                    "3,3",
+                    "2,6",
+                    "5,1",
+                    "1,2",
+                    "5,5",
+                    "2,5",
+                    "6,5",
+                    "1,4",
+                    "0,4",
+                    "6,4",
+                    "1,1",
+                    "6,1",
+                    "1,0",
+                    "0,5",
+                    "1,6",
+                    "2,0",
                 ],
-                Result = 8,
+                Result = 22,
             },
             Examples = [
             ],
