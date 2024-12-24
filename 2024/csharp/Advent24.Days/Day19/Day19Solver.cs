f789edfcd4438d0746f2ae0279e3035dc5944594
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-24 13:39:31 -0600

    Adding 2024 Day 19 in C#

---
+++ 2024/csharp/Advent24.Days/Day19/Day19Solver.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Commons;
+
+namespace Advent24.Days
+{
+    public class Day19Solver : Solver<object[], long>
+    {
+        public Day19Solver() : base(2024, 19) { }
+
+        public override object[] ParseInput(IEnumerable<string> input)
+            => input.ToArray();
+
+
+        public override long PartOne(object[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long PartTwo(object[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
