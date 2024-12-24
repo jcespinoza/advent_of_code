3cd01a8150e6cd4b904981ed50e2043883c71bce
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-24 15:02:55 -0600

    Completed Part One

--- 2024/csharp/Advent24.Days/Day19/Day19Solver.cs
+++ 2024/csharp/Advent24.Days/Day19/Day19Solver.cs
@@ -2,22 +2,88 @@
 
 namespace Advent24.Days
 {
-    public class Day19Solver : Solver<object[], long>
+    public class Day19Solver : Solver<OnsenWarehouse, long>
     {
         public Day19Solver() : base(2024, 19) { }
 
-        public override object[] ParseInput(IEnumerable<string> input)
-            => input.ToArray();
+        public override OnsenWarehouse ParseInput(IEnumerable<string> input)
+        {
+            var availability = input
+                .First()
+                .Split(", ")
+                .ToList();
+
+            var patterns
+                = input
+                .Skip(2)
+                .ToList();
+
+            return new()
+            {
+                Patterns = availability,
+                Designs = patterns
+            };
+        }
 
 
-        public override long PartOne(object[] input)
+        public override long PartOne(OnsenWarehouse warehouse)
+        {
+            List<string> designs = GetFeasibleDesigns(warehouse);
+
+            return designs.Count;
+        }
+
+        private List<string> GetFeasibleDesigns(OnsenWarehouse warehouse)
+        {
+            Dictionary<string, bool> feasibilityCache = [];
+            var feasibleDesigns = new List<string>();
+            var maxPatterLength = warehouse.Patterns.Max(x => x.Length);
+
+            foreach (var design in warehouse.Designs) {
+                var possible = IsDesignPossible(design, warehouse.Patterns, feasibilityCache, maxPatterLength);
+                if (possible)
+                {
+                    feasibleDesigns.Add(design);
+                }
+            }
+            return feasibleDesigns;
+        }
+
+        private static bool IsDesignPossible(string design, List<string> patterns, Dictionary<string, bool> feasibilityCache, int maxPatterLength)
+        {
+            if(feasibilityCache.TryGetValue(design, out bool cachedResult))
+            {
+                return cachedResult;
+            }
+
+            if(design.Length == 0)
+            {
+                return true;
+            }
+            var maxLength = Math.Min(design.Length, maxPatterLength);
+            foreach (var countToGrab in Enumerable.Range(0, maxLength +1 ))
+            {
+                bool patterExists = patterns.Contains(design[..countToGrab]);
+                bool subPatternFeasible = IsDesignPossible(design[countToGrab..], patterns, feasibilityCache, maxPatterLength);
+                if (patterExists && subPatternFeasible)
+                {
+                    feasibilityCache.Add(design, true);
+                    return true;
+                }
+            }
+            feasibilityCache.Add(design, false);
+            return false;
+        }
+
+        public override long PartTwo(OnsenWarehouse warehouse)
         {
             throw new NotImplementedException();
         }
+    }
 
-        public override long PartTwo(object[] input)
-        {
-            throw new NotImplementedException();
-        }
+    public class OnsenWarehouse
+    {
+        public List<string> Patterns { get; set; } = [];
+        public List<string> Designs { get; set; } = default!;
     }
 }
--- 2024/csharp/Advent24.Days/Day19/Day19Test.cs
+++ 2024/csharp/Advent24.Days/Day19/Day19Test.cs
@@ -3,9 +3,9 @@ using dotenv.net;
 
 namespace Advent24.Days
 {
-    public class Day19Test : TestEngine<Day19Solver, object[], long>
+    public class Day19Test : TestEngine<Day19Solver, OnsenWarehouse, long>
     {
-        private const long EXPECTED_SOLUTION_PART_1 = 123;
+        private const long EXPECTED_SOLUTION_PART_1 = 344;
         private const long EXPECTED_SOLUTION_PART_2 = 123;
 
         public Day19Test()
@@ -15,12 +15,22 @@ namespace Advent24.Days
 
         public override Puzzle PartOne => new()
         {
-            ShouldSkipTests = true,
+            ShouldSkipTests = false,
             Example = new()
             {
                 RawInput = [
+                    "r, wr, b, g, bwu, rb, gb, br",
+                    "",
+                    "brwrr",
+                    "bggr",
+                    "gbbr",
+                    "rrbgbr",
+                    "ubwu",
+                    "bwurrg",
+                    "brgr",
+                    "bbrgwb",
                 ],
-                Result = 8,
+                Result = 6,
             },
             Examples = [
             ],
