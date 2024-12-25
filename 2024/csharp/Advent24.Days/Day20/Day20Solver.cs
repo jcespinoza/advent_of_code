e94897de88ceef89ef185303d7d38aa0f681a7b8
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-25 09:23:10 -0600

    Adding 2024 Day 20 in C# (#48)
    * Adding 2024 Day 20 in C#
    
    * Adding method to compute the racetrack path
    
    * Not workin
    
    * Completed Part One by adding starting point to the path
    
    * Adjustments for Part 2
    
    * Adding similar method
    
    * Minor adjustments

---
+++ 2024/csharp/Advent24.Days/Day20/Day20Solver.cs
@@ -0,0 +1,174 @@
+﻿using AdventOfCode.Commons;
+
+namespace Advent24.Days
+{
+    public class Day20Solver : SteppedSolver<char[][], char[][], long, long>
+    {
+        public Day20Solver() : base(2024, 20) { }
+
+        public override char[][] ParseInputOne(IEnumerable<string> input)
+            => input
+            .Select(l => l.ToCharArray())
+            .ToArray();
+        
+        public override char[][] ParseInputTwo(IEnumerable<string> input)
+            => ParseInputOne(input);
+
+        public override long PartOne(char[][] map)
+        {
+            int targetTimeSaved = 100;
+
+            var path = FindPath(map);
+
+            long cheats = FindCheatsForPicosends(map, path, targetTimeSaved, useExact: false);
+
+            return cheats;
+        }
+
+        public override long PartTwo(char[][] map)
+        {
+            int targetTimeSaved = 100;
+
+            var path = FindPath(map);
+
+            long cheats = FindAdvancedCheatsForPicosends(map, path, targetTimeSaved, useExact: false);
+
+            return cheats;
+        }
+
+        public static long FindCheatsForPicosends(char[][] map, List<(int, int)> path, int targetTimeSaved, bool useExact)
+        {
+            int offset = 2;
+            int count = 0;
+            foreach ((int row, int col) in path) {
+                foreach ((int nRow, int nCol) in new (int, int)[] {
+                    (row - offset, col),
+                    (row + offset, col),
+                    (row, col - offset),
+                    (row, col + offset)
+                })
+                {
+                    if (nRow < 0 || nRow >= map.Length || nCol < 0 || nCol >= map[nRow].Length) continue;
+                    if (map[nRow][nCol] == '#') continue;
+                    if (map[nRow][nCol] == 'S') continue;
+
+                    var distA = path.IndexOf((row, col));
+                    var distB = path.IndexOf((nRow, nCol));
+                    if (distA > distB) continue;
+                    int localDist = Math.Abs(distA - distB);
+                    bool savesEqualOrMoreSteps = localDist >= targetTimeSaved + offset;
+                    bool savesEqualSteps = localDist == targetTimeSaved + offset;
+                    bool shouldCount = useExact ? savesEqualSteps : savesEqualOrMoreSteps;
+
+                    if (shouldCount)
+                    {
+                        count++;
+                    }
+                }
+                
+            }
+            return count;
+        }
+
+        public static long FindAdvancedCheatsForPicosends(char[][] map, List<(int, int)> path, int targetTimeSaved, bool useExact)
+        {
+            int maxTime = 20;
+            int count = 0;
+            foreach ((int row, int col) in path)
+            {
+                if (map[row][col] == '#') continue;
+                for (int offset = 2; offset <= maxTime; offset++)
+                {
+                    foreach ((int nRow, int nCol) in new (int, int)[] {
+                        (row - offset, col),
+                        (row + offset, col),
+                        (row, col - offset),
+                        (row, col + offset)
+                    })
+                    {
+                        if (nRow < 0 || nRow >= map.Length || nCol < 0 || nCol >= map[nRow].Length) continue;
+                        if (map[nRow][nCol] == '#') continue;
+                        if (map[nRow][nCol] == 'S') continue;
+
+                        var distA = path.IndexOf((row, col));
+                        var distB = path.IndexOf((nRow, nCol));
+                        if (distA > distB) continue;
+                        int localDist = Math.Abs(distA - distB);
+                        bool savesEqualOrMoreSteps = localDist >= targetTimeSaved + offset;
+                        bool savesEqualSteps = localDist == targetTimeSaved + offset;
+                        bool shouldCount = useExact ? savesEqualSteps : savesEqualOrMoreSteps;
+
+                        if (shouldCount)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+
+        private static void AddIfNotInList(HashSet<(int, int, int, int)> cheats, (int, int) start, (int, int) end)
+        {
+            (int sRow, int sCol) = start;
+            (int eRow, int eCol) = end;
+            if (cheats.Contains((sRow, sCol, eRow, eCol))) return;
+            if (cheats.Contains((eRow, eCol, sRow, sCol))) return;
+
+            cheats.Add((sRow, sCol, eRow, eCol));
+        }
+
+        public static List<(int, int)> FindPath(char[][] map)
+        {
+            // Starting at 'S' and ending at 'E', find all the (row, column) that are part of the path
+            (int, int) start = FindInMap(map, 'S');
+            (int, int) goal = FindInMap(map, 'E');
+
+            List<(int, int)> path = [start];
+
+            (int cRow, int cCol) = start;
+            while ((cRow, cCol) != goal)
+            {
+                // Find the next step
+                (int nRow, int nCol) = FindNextStep(map, cRow, cCol, path);
+                path.Add((nRow, nCol));
+                (cRow, cCol) = (nRow, nCol);
+            }
+            return path;
+        }
+
+        private static (int nRow, int nCol) FindNextStep(char[][] map, int cRow, int cCol, List<(int, int)> path)
+        {
+            foreach ((int nRow, int nCol) in new (int,int)[]{
+                (cRow - 1, cCol),
+                (cRow + 1, cCol),
+                (cRow, cCol - 1),
+                (cRow, cCol + 1)
+            })
+            {
+                char cellValue = map[nRow][nCol];
+                if (path.Contains((nRow, nCol))) continue;
+                if (cellValue == '.' || cellValue == 'E')
+                {
+                    return (nRow, nCol);
+                }
+            }
+            return (-1, -1);
+        }
+
+        private static (int, int) FindInMap(char[][] map, char v)
+        {
+            for (int row = 0; row < map.Length; row++)
+            {
+                for (int col = 0; col < map[row].Length; col++)
+                {
+                    if (map[row][col] == v)
+                    {
+                        return (row, col);
+                    }
+                }
+            }
+            return (-1, -1);
+        }
+    }
+}
---
+++ 2024/csharp/Advent24.Days/Day20/RacingSolverTests.cs
@@ -0,0 +1,175 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Advent24.Days.Day20
+{
+    public class RacingSolverTests
+    {
+        Day20Solver _solver;
+        readonly IEnumerable<string> _rawInput = [
+                "###############",
+                "#...#...#.....#",
+                "#.#.#.#.#.###.#",
+                "#S#...#.#.#...#",
+                "#######.#.#.###",
+                "#######.#.#...#",
+                "#######.#.###.#",
+                "###..E#...#...#",
+                "###.#######.###",
+                "#...###...#...#",
+                "#.#####.#.###.#",
+                "#.#...#.#.#...#",
+                "#.#.#.#.#.#.###",
+                "#...#...#...###",
+                "###############",
+                ];
+        readonly char[][] _racetrack;
+
+        public RacingSolverTests()
+        {
+            _solver = new();
+            _racetrack = _solver.ParseInputOne(_rawInput);
+        }
+
+        [Fact]
+        public void FindPathLength_Test01()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+            var expectedLength = 85;
+
+            path.Count.Should().Be(expectedLength);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test2_14()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 2, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(14);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test4_14()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 4, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(14);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test6_2()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 6, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(2);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test8_4()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 8, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(4);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test10_2()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 10, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(2);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test12_3()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 12, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(3);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test20_1()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 20, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(1);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test36_1()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 36, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(1);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test38_1()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 38, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(1);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test40_1()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 40, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(1);
+        }
+
+        [Fact]
+        public void FindCheatsForPicosends_Test64_1()
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+
+            long cheatsForPicoseconds = Day20Solver.FindCheatsForPicosends(_racetrack, path, targetTimeSaved: 64, useExact: true);
+
+            cheatsForPicoseconds.Should().Be(1);
+        }
+
+        [Theory]
+        [InlineData(50, 32)]
+        [InlineData(52, 31)]
+        [InlineData(54, 29)]
+        [InlineData(56, 39)]
+        [InlineData(58, 25)]
+        [InlineData(60, 23)]
+        [InlineData(62, 20)]
+        [InlineData(64, 19)]
+        [InlineData(66, 12)]
+        [InlineData(68, 14)]
+        [InlineData(70, 12)]
+        [InlineData(72, 22)]
+        [InlineData(74, 4)]
+        [InlineData(76, 3)]
+        public void FindAdvancedCheatsForPicosends_Test(int targetTimeSaved, int expectedCheats)
+        {
+            var path = Day20Solver.FindPath(_racetrack);
+            long cheatsForPicoseconds = Day20Solver.FindAdvancedCheatsForPicosends(_racetrack, path, targetTimeSaved, useExact: true);
+            cheatsForPicoseconds.Should().Be(expectedCheats);
+        }
+    }
+}
