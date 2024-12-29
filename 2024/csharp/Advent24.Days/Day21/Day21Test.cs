4506157eea3ce9288b0de9a60d36938ff1ea0546
Juan Carlos Espinoza <dev@jcespinoza.com>
2024-12-29 16:44:03 -0600

    Completed Part One

--- 2024/csharp/Advent24.Days/Day21/Day21Test.cs
+++ 2024/csharp/Advent24.Days/Day21/Day21Test.cs
@@ -3,9 +3,9 @@ using dotenv.net;
 
 namespace Advent24.Days
 {
-    public class Day21Test : TestEngine<Day21Solver, object[], long>
+    public class Day21Test : TestEngine<Day21Solver, string[], long>
     {
-        private const long EXPECTED_SOLUTION_PART_1 = 123;
+        private const long EXPECTED_SOLUTION_PART_1 = 202274;
         private const long EXPECTED_SOLUTION_PART_2 = 123;
 
         public Day21Test()
@@ -15,12 +15,17 @@ namespace Advent24.Days
 
         public override Puzzle PartOne => new()
         {
-            ShouldSkipTests = true,
+            ShouldSkipTests = false,
             Example = new()
             {
                 RawInput = [
+                    "029A",
+                    "980A",
+                    "179A",
+                    "456A",
+                    "379A",
                 ],
-                Result = 8,
+                Result = 126384,
             },
             Examples = [
             ],
