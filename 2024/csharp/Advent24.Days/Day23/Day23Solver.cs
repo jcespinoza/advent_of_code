1924ffa927f732db68f6ca5947b903a7a6ccaf5e
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-02 11:23:59 -0600

    Adding 2024 Day 23 in C#

---
+++ 2024/csharp/Advent24.Days/Day23/Day23Solver.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Commons;
+
+namespace Advent24.Days
+{
+    public class Day23Solver : Solver<object[], long>
+    {
+        public Day23Solver() : base(2024, 23) { }
+
+        public override object[] ParseInput(IEnumerable<string> input)
+            => input.ToArray();
+
+
+        public override long PartOne(object[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long PartTwo(object[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
