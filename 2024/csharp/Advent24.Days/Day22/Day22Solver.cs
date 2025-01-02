bc0ca1b49ae3dc44a039d269ebd639a7b8b362a1
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-01 20:27:59 -0600

    Completed Part One

--- 2024/csharp/Advent24.Days/Day22/Day22Solver.cs
+++ 2024/csharp/Advent24.Days/Day22/Day22Solver.cs
@@ -2,20 +2,46 @@
 
 namespace Advent24.Days
 {
-    public class Day22Solver : Solver<object[], long>
+    public class Day22Solver : Solver<int[], long>
     {
+        private const int PRUNE_FACTOR = 16777216;
         public Day22Solver() : base(2024, 22) { }
 
-        public override object[] ParseInput(IEnumerable<string> input)
-            => input.ToArray();
+        public override int[] ParseInput(IEnumerable<string> input)
+            => input.Select(int.Parse).ToArray();
 
 
-        public override long PartOne(object[] input)
+        public override long PartOne(int[] initialSecrets)
         {
-            throw new NotImplementedException();
+            int stepOfInterest = 2000;
+            long totalSum = 0;
+            foreach (int secretNumber in initialSecrets)
+            {
+                long nthSecret = FindNthSecretNumber(initial: secretNumber, stepOfInterest);
+                totalSum += nthSecret;
+            }
+            return totalSum;
         }
 
-        public override long PartTwo(object[] input)
+        public static long FindNthSecretNumber(int initial, int stepOfInterest)
+        {
+            long currentSecret = initial;
+            for (int step = 0; step < stepOfInterest; step++)
+            {
+                long prod = currentSecret * 64;
+                currentSecret ^= prod;
+                currentSecret %= PRUNE_FACTOR;
+                long quotient = currentSecret / 32;
+                currentSecret ^= quotient;
+                currentSecret %= PRUNE_FACTOR;
+                prod = currentSecret * 2048;
+                currentSecret ^= prod;
+                currentSecret %= PRUNE_FACTOR;
+            }
+            return currentSecret;
+        }
+
+        public override long PartTwo(int[] initialSecrets)
         {
             throw new NotImplementedException();
         }
--- 2024/csharp/Advent24.Days/Day22/Day22Test.cs
+++ 2024/csharp/Advent24.Days/Day22/Day22Test.cs
@@ -1,11 +1,12 @@
 ﻿using AdventOfCode.Commons;
 using dotenv.net;
+using Xunit;
 
 namespace Advent24.Days
 {
-    public class Day22Test : TestEngine<Day22Solver, object[], long>
+    public class Day22Test : TestEngine<Day22Solver, int[], long>
     {
-        private const long EXPECTED_SOLUTION_PART_1 = 123;
+        private const long EXPECTED_SOLUTION_PART_1 = 19822877190L;
         private const long EXPECTED_SOLUTION_PART_2 = 123;
 
         public Day22Test()
@@ -15,12 +16,16 @@ namespace Advent24.Days
 
         public override Puzzle PartOne => new()
         {
-            ShouldSkipTests = true,
+            ShouldSkipTests = false,
             Example = new()
             {
                 RawInput = [
+                    "1",
+                    "10",
+                    "100",
+                    "2024",
                 ],
-                Result = 8,
+                Result = 37327623,
             },
             Examples = [
             ],
@@ -38,5 +43,25 @@ namespace Advent24.Days
             },
             Solution = EXPECTED_SOLUTION_PART_2,
         };
+
+        [Theory]
+        [InlineData(123, 1, 15887950)]
+        [InlineData(123, 2, 16495136)]
+        [InlineData(123, 3, 527345)]
+        [InlineData(123, 4, 704524)]
+        [InlineData(123, 5, 1553684)]
+        [InlineData(123, 6, 12683156)]
+        [InlineData(123, 7, 11100544)]
+        [InlineData(123, 8, 12249484)]
+        [InlineData(123, 9, 7753432)]
+        [InlineData(123, 10, 5908254)]
+        public void FindNthSecretNumber_Test(int initial, int step, long expected)
+        {
+            // Arrange
+            // Act
+            long result = Day22Solver.FindNthSecretNumber(initial, step);
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
