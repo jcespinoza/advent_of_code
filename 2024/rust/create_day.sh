8e93722c238b31bd3280ff1591d8c6f3df1eb1a1
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-10 15:35:58 -0600

    Updating script

--- 2024/rust/create_day.sh
+++ 2024/rust/create_day.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Function to perform cleanup actions
+rollback() {
+    echo "An error occurred. Rolling back changes..."
+    # Add your rollback commands here
+    # For example, you can remove files or undo changes made by the script
+    git reset --hard HEAD
+    git clean -fd
+}
+
+# Trap errors and call the rollback function
+trap rollback ERR
+
 # Check that the operating system is either windows or linux
 echo "Checking operating system..."
 if [ "$(uname)" == "Darwin" ]; then
@@ -61,93 +77,71 @@ echo "Appending module definitions to src/days/mod.rs"
 cat templates/mod.rs.txt >> src/days/mod.rs
 
 cd src/days # Go to the days folder
-echo "Entering Days folder"
 
 # Replace all instances of 00 with DAYNAME in the mod.rs file
 sed -i "s/00/$DAYNAME/g" mod.rs
-echo "Replaced all instances of 00 with $DAYNAME in mod.rs"
 
 cd day$DAYNAME
-echo "Entering day$DAYNAME folder"
 
+echo "Renaming and updating template files to match day number"
 # Rename the day00_impl.rs file to day<DAYNAME>_impl.rs
 mv day00_impl.rs day${DAYNAME}_impl.rs
-echo "Renamed day00_impl.rs to day${DAYNAME}_impl.rs"
 
 # Rename the day00_lib.rs file to day<DAYNAME>_lib.rs
 mv day00_lib.rs day${DAYNAME}_lib.rs
-echo "Renamed day00_lib.rs to day${DAYNAME}_lib.rs"
 
 # Rename the day00_solver.rs file to day<DAYNAME>_solver.rs
 mv day00_solver.rs day${DAYNAME}_solver.rs
-echo "Renamed day00_solver.rs to day${DAYNAME}_solver.rs"
 
 # Rename the day00_tests.rs file to day<DAYNAME>_tests.rs
 mv day00_tests.rs day${DAYNAME}_tests.rs
-echo "Renamed day00_tests.rs to day${DAYNAME}_tests.rs"
 
 # Replace all instances of 00 with DAYNAME in the day<DAYNAME>_impl.rs file
 sed -i "s/00/$DAYNAME/g" day${DAYNAME}_impl.rs
-echo "Replaced all instances of 00 with $DAYNAME in day${DAYNAME}_impl.rs"
 
 # Replace all instances of 00 with DAYNAME in the day<DAYNAME>_lib.rs file
 sed -i "s/00/$DAYNAME/g" day${DAYNAME}_lib.rs
-echo "Replaced all instances of 00 with $DAYNAME in day${DAYNAME}_lib.rs"
 
 # Replace all instances of 00 with DAYNAME in the day<DAYNAME>_solver.rs file
 sed -i "s/00/$DAYNAME/g" day${DAYNAME}_solver.rs
-echo "Replaced all instances of 00 with $DAYNAME in day${DAYNAME}_solver.rs"
 
 # Replace all instances of 00 with DAYNAME in the day<DAYNAME>_tests.rs file
 sed -i "s/00/$DAYNAME/g" day${DAYNAME}_tests.rs
-echo "Replaced all instances of 00 with $DAYNAME in day${DAYNAME}_tests.rs"
 sed -i "s/DAY_NUM: i32 = 0/DAY_NUM: i32 = $DAY/g" day${DAYNAME}_tests.rs
-echo "Replaced value in DAY_NUM with $DAY in day${DAYNAME}_tests.rs"
 
 cd .. # Go back to the days folder
-echo "Exiting day$DAYNAME folder"
 cd .. # Go back to the src folder
-echo "Exiting days folder"
 
 cd integration # Go to the integration folder
-echo "Entering integration folder"
 
-
-echo "Appending imports to solver_mapping.rs"
+echo "Registering day number to a solver in solver_mapping.rs"
 # Adding new import for the new day solver
 BLANKS="  "
 sed -i "/\/\/NEXT_IMPORT/i\\
 ${BLANKS}days::day${DAYNAME}::Day${DAYNAME}Solver," solver_mapping.rs
 
-echo "Appending new DayNum entry"
 # Adding new enum entry
 BLANKS="  "
 sed -i "/\/\/NEXT_ENUM_ENTRY/i\\
 ${BLANKS}Day${DAYNAME} = ${DAY}," solver_mapping.rs
 
-echo "Appending new DayNum match entry"
 # Adding new enum try_from entry
 BLANKS="      "
 sed -i "/\/\/NEXT_ENUM_TRY_FROM/i\\
 ${BLANKS}x if x == DayNum::Day${DAYNAME} as i32 => Ok(DayNum::Day${DAYNAME})," solver_mapping.rs
 
-echo "Appending DayNum mapping for the new solver"
 # Adding new enum match entry
 BLANKS="    "
 sed -i "/\/\/NEXT_ENUM_MATCH/i\\
 ${BLANKS}DayNum::Day${DAYNAME} => Box::new(Day${DAYNAME}Solver { day: ${DAY}, year })," solver_mapping.rs
 
 cd .. # Go back to the src folder
-echo "Exiting days folder"
 cd .. # Go back to the root folder
-echo "Exiting src folder"
 
 # Create a branch and a commit for the new day
-echo "Creating a branch and a commit for the new day"
+echo "\nCreating a branch and a commit for the new day"
 git checkout -b day-$DAYNAME-rust24
 git add .
-echo "Added all files to the commit"
-echo "Committing changes..."
 git commit -m "Adding 2024 Day $DAYNAME in Rust"
 echo "Changes committed. Ready to publish the branch"
 echo "Start coding for Day $DAY!"
