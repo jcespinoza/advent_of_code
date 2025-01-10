a505fb5c81ab3a444f4c65ce8d8a8ebb9bce6c63
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-10 15:22:26 -0600

    Completed create_day script

--- 2024/rust/create_day.sh
+++ 2024/rust/create_day.sh
@@ -57,7 +57,7 @@ echo "Copying from template"
 cp -r templates/day-template src/days/day$DAYNAME
 echo "Copied day-template to src/days/day$DAYNAME"
 # Append the contents of templates/mod.rs.txt to the end of src/days/mod.rs
-echo "Appending to src/days/mod.rs"
+echo "Appending module definitions to src/days/mod.rs"
 cat templates/mod.rs.txt >> src/days/mod.rs
 
 cd src/days # Go to the days folder
@@ -100,8 +100,9 @@ echo "Replaced all instances of 00 with $DAYNAME in day${DAYNAME}_solver.rs"
 
 # Replace all instances of 00 with DAYNAME in the day<DAYNAME>_tests.rs file
 sed -i "s/00/$DAYNAME/g" day${DAYNAME}_tests.rs
-sed -i "s/DAY_NUM: i32 = 0/DAY_NUM: i32 = $DAY/g" day${DAYNAME}_tests.rs
 echo "Replaced all instances of 00 with $DAYNAME in day${DAYNAME}_tests.rs"
+sed -i "s/DAY_NUM: i32 = 0/DAY_NUM: i32 = $DAY/g" day${DAYNAME}_tests.rs
+echo "Replaced value in DAY_NUM with $DAY in day${DAYNAME}_tests.rs"
 
 cd .. # Go back to the days folder
 echo "Exiting day$DAYNAME folder"
@@ -111,16 +112,30 @@ echo "Exiting days folder"
 cd integration # Go to the integration folder
 echo "Entering integration folder"
 
+
+echo "Appending imports to solver_mapping.rs"
 # Adding new import for the new day solver
 BLANKS="  "
-sed -i "/\/\/NEXT_IMPORT/i ${BLANKS}days::day$DAYNAME::Day${DAYNAME}Solver," solver_mapping.rs
-# Adding new enum entry
-sed -i "/\/\/NEXT_ENUM_ENTRY/i ${BLANKS}Day$DAYNAME = $DAY," solver_mapping.rs
-BLANKS="      "
-sed -i "/\/\/NEXT_ENUM_TRY_FROM/i ${BLANKS}x if x == DayNum::Day$DAYNAME as i32 => Ok(DayNum::Day$DAYNAME)," solver_mapping.rs
-BLANKS="    "
-sed -i "/\/\/NEXT_ENUM_MATCH/i ${BLANKS}DayNum::Day$DAYNAME => Box::new(Day${DAYNAME}Solver { day: $DAY, year })," solver_mapping.rs
+sed -i "/\/\/NEXT_IMPORT/i\\
+${BLANKS}days::day${DAYNAME}::Day${DAYNAME}Solver," solver_mapping.rs
 
+echo "Appending new DayNum entry"
+# Adding new enum entry
+BLANKS="  "
+sed -i "/\/\/NEXT_ENUM_ENTRY/i\\
+${BLANKS}Day${DAYNAME} = ${DAY}," solver_mapping.rs
+
+echo "Appending new DayNum match entry"
+# Adding new enum try_from entry
+BLANKS="      "
+sed -i "/\/\/NEXT_ENUM_TRY_FROM/i\\
+${BLANKS}x if x == DayNum::Day${DAYNAME} as i32 => Ok(DayNum::Day${DAYNAME})," solver_mapping.rs
+
+echo "Appending DayNum mapping for the new solver"
+# Adding new enum match entry
+BLANKS="    "
+sed -i "/\/\/NEXT_ENUM_MATCH/i\\
+${BLANKS}DayNum::Day${DAYNAME} => Box::new(Day${DAYNAME}Solver { day: ${DAY}, year })," solver_mapping.rs
 
 cd .. # Go back to the src folder
 echo "Exiting days folder"
