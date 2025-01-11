fc00639d81622301a2ea911aa0fd4453eaaa6f60
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-11 07:48:19 -0600

    Small adjustment to support puzzle input that is

--- 2015/rust/src/main.rs
+++ 2015/rust/src/main.rs
@@ -5,19 +5,24 @@ use advent_of_code::{
 
 fn main() {
   let args: Vec<String> = std::env::args().collect();
-  if args.len() < 5 {
-    eprintln!("Usage: advent_of_code --day <day> --part <part>");
+  if args.len() < 7 {
+    eprintln!("Usage: advent_of_code --day <day> --part <part> [--input <input>]");
     std::process::exit(1);
   }
 
   let mut day = 0;
   let mut part = 0;
+  let mut inline_input: &str = "";
+  let mut inline_input_provided = false;
 
   for i in 0..args.len() {
     if args[i] == "--day" || args[i] == "-d" {
       day = args[i + 1].parse::<i32>().unwrap();
     } else if args[i] == "--part" || args[i] == "-p" {
       part = args[i + 1].parse::<i32>().unwrap();
+    } else if args[i] == "--input" || args[i] == "-i" {
+      inline_input_provided = true;
+      inline_input = &args[i + 1];
     }
   }
 
@@ -26,13 +31,18 @@ fn main() {
     std::process::exit(1);
   }
 
-  let input_provider = PuzzleInputProvider::new_remote(YEAR_NUM, day);
-  let input = input_provider.read_input().unwrap();
+  let mut puzzle_input = vec![];
+  if inline_input_provided {
+    puzzle_input.push(inline_input.to_string());
+  } else {
+    let input_provider = PuzzleInputProvider::new_remote(YEAR_NUM, day);
+    puzzle_input = input_provider.read_input().unwrap();
+  }
 
   let solver = create_solver(YEAR_NUM, day);
   let result = match part {
-    1 => solver.solve_part_one_str(input.iter().map(|x| x.as_str()).collect()),
-    2 => solver.solve_part_two_str(input.iter().map(|x| x.as_str()).collect()),
+    1 => solver.solve_part_one_str(puzzle_input.iter().map(|x| x.as_str()).collect()),
+    2 => solver.solve_part_two_str(puzzle_input.iter().map(|x| x.as_str()).collect()),
     _ => panic!("Invalid part number"),
   };
 
