f47e678d4088da9c11a8fd2d8d9813abb6f5a5cc
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-11 11:26:02 -0600

    Completed Part Two

--- 2015/rust/src/days/day05/day05_solver.rs
+++ 2015/rust/src/days/day05/day05_solver.rs
@@ -17,39 +17,86 @@ impl SteppedSolver<Vec<String>, Vec<String>, i64, i64> for Day05Solver {
   }
 
   fn solve_part_one(&self, input: Vec<String>) -> i64 {
-    let count_nice = input.iter().filter(|x| is_nice(x)).count();
+    let count_nice = input
+      .iter()
+      .filter(|string_element| {
+        let mut vowels = 0;
+        let mut has_duplicate = false;
+        let mut last_char = ' ';
+
+        for c_char in string_element.chars() {
+          match c_char {
+            'a' | 'e' | 'i' | 'o' | 'u' => vowels += 1,
+            _ => (),
+          }
+
+          if c_char == last_char {
+            has_duplicate = true;
+          }
+
+          if (last_char == 'a' && c_char == 'b')
+            || (last_char == 'c' && c_char == 'd')
+            || (last_char == 'p' && c_char == 'q')
+            || (last_char == 'x' && c_char == 'y')
+          {
+            return false;
+          }
+
+          last_char = c_char;
+        }
+        vowels >= 3 && has_duplicate
+      })
+      .count();
     count_nice as i64
   }
 
   fn solve_part_two(&self, input: Vec<String>) -> i64 {
-    unimplemented!()
+    let count_nice = input
+      .iter()
+      .filter(|string_element| {
+        let has_non_overlapping_pairs = has_overlapping_pairs(string_element);
+        let has_repeated_letter = has_repeated_letter(string_element);
+        has_non_overlapping_pairs && has_repeated_letter
+      })
+      .count();
+    count_nice as i64
   }
 }
 
-fn is_nice(x: &str) -> bool {
-  let mut vowels = 0;
-  let mut has_duplicate = false;
+fn has_overlapping_pairs(string_element: &str) -> bool {
+  for (index, target_second) in string_element.chars().enumerate() {
+    // Skip the first character
+    if index < 1 {
+      continue;
+    }
+
+    let target_first = string_element.chars().nth(index - 1).unwrap();
+
+    for (index2, match_second) in string_element.chars().enumerate() {
+      // Skip up to the target pair
+      if index2 <= index + 1 {
+        continue;
+      }
+      let match_first = string_element.chars().nth(index2 - 1).unwrap();
+      if target_first == match_first && target_second == match_second {
+        return true;
+      }
+    }
+  }
+  false
+}
+
+fn has_repeated_letter(string_element: &str) -> bool {
+  let mut last_char_2behind = ' ';
   let mut last_char = ' ';
 
-  for c_char in x.chars() {
-    match c_char {
-      'a' | 'e' | 'i' | 'o' | 'u' => vowels += 1,
-      _ => (),
+  for current_char in string_element.chars() {
+    if (current_char == last_char_2behind) {
+      return true;
     }
 
-    if c_char == last_char {
-      has_duplicate = true;
-    }
-
-    if (last_char == 'a' && c_char == 'b')
-      || (last_char == 'c' && c_char == 'd')
-      || (last_char == 'p' && c_char == 'q')
-      || (last_char == 'x' && c_char == 'y')
-    {
-      return false;
-    }
-
-    last_char = c_char;
+    last_char_2behind = last_char;
+    last_char = current_char;
   }
-  vowels >= 3 && has_duplicate
+  false
 }
--- 2015/rust/src/days/day05/day05_tests.rs
+++ 2015/rust/src/days/day05/day05_tests.rs
@@ -46,10 +46,14 @@ fn solution_part_one() {
 }
 
 #[test]
-#[ignore]
 fn sample_01_part_two() {
-  let raw_input = vec![];
-  let expected_output: i64 = 0;
+  let raw_input = vec![
+    "qjhvhtzxzqqjkmpb",
+    "xxyxx",
+    "uurcxstgmygtbstg",
+    "ieodomkazucvgmuy",
+  ];
+  let expected_output: i64 = 2;
 
   let solver = Day05Solver {
     day: DAY_NUM,
@@ -62,13 +66,12 @@ fn sample_01_part_two() {
 }
 
 #[test]
-#[ignore]
 fn solution_part_two() {
   let remote_input = PuzzleInputProvider::new_remote(YEAR_NUM, DAY_NUM)
     .read_input()
     .unwrap();
   let raw_input = remote_input.iter().map(|x| x.as_str()).collect();
-  const EXPECTED_SOLUTION_PART2: i64 = 0;
+  const EXPECTED_SOLUTION_PART2: i64 = 53;
 
   let solver = Day05Solver {
     day: DAY_NUM,
@@ -78,29 +81,4 @@ fn solution_part_two() {
   let result = solver.solve_part_two(input);
 
   assert_eq!(result, EXPECTED_SOLUTION_PART2);
-}
-
-#[test]
-#[ignore]
-fn examples_part_two() {
-  let examples: Vec<AocExample<i64>> = vec![AocExample {
-    raw_input: vec![],
-    expected_output: 0,
-  }];
-
-  let solver = Day05Solver {
-    day: DAY_NUM,
-    year: YEAR_NUM,
-  };
-
-  for (index, example) in examples.iter().enumerate() {
-    let input = solver.parse_input_two(example.raw_input.clone());
-    let result = solver.solve_part_two(input);
-
-    assert_eq!(
-      result, example.expected_output,
-      "Failed for Example: {:?}",
-      index
-    );
-  }
 }
