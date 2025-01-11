ade5c9948c6fb01adf86bef3fd75661cde90b44d
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-11 08:55:27 -0600

    Completed Part One

--- 2015/rust/src/days/day04/day04_tests.rs
+++ 2015/rust/src/days/day04/day04_tests.rs
@@ -22,41 +22,6 @@ fn sample_01_part_one() {
 }
 
 #[test]
-#[ignore]
-fn sample_01_part_two() {
-  let raw_input = vec![];
-  let expected_output: i64 = 0;
-
-  let solver = Day04Solver {
-    day: DAY_NUM,
-    year: YEAR_NUM,
-  };
-  let input = solver.parse_input_two(raw_input);
-  let result = solver.solve_part_two(input);
-
-  assert_eq!(result, expected_output);
-}
-
-#[test]
-#[ignore]
-fn solution_part_two() {
-  let remote_input = PuzzleInputProvider::new_remote(YEAR_NUM, DAY_NUM)
-    .read_input()
-    .unwrap();
-  let raw_input = remote_input.iter().map(|x| x.as_str()).collect();
-  const EXPECTED_SOLUTION_PART2: i64 = 0;
-
-  let solver = Day04Solver {
-    day: DAY_NUM,
-    year: YEAR_NUM,
-  };
-  let input = solver.parse_input_two(raw_input);
-  let result = solver.solve_part_two(input);
-
-  assert_eq!(result, EXPECTED_SOLUTION_PART2);
-}
-
-#[test]
 fn examples_part_one() {
   let examples: Vec<AocExample<i64>> = vec![AocExample {
     raw_input: vec!["pqrstuv"],
@@ -71,31 +36,6 @@ fn examples_part_one() {
   for (index, example) in examples.iter().enumerate() {
     let input = solver.parse_input_one(example.raw_input.clone());
     let result = solver.solve_part_one(input);
-
-    assert_eq!(
-      result, example.expected_output,
-      "Failed for Example: {:?}",
-      index
-    );
-  }
-}
-
-#[test]
-#[ignore]
-fn examples_part_two() {
-  let examples: Vec<AocExample<i64>> = vec![AocExample {
-    raw_input: vec![],
-    expected_output: 0,
-  }];
-
-  let solver = Day04Solver {
-    day: DAY_NUM,
-    year: YEAR_NUM,
-  };
-
-  for (index, example) in examples.iter().enumerate() {
-    let input = solver.parse_input_two(example.raw_input.clone());
-    let result = solver.solve_part_two(input);
 
     assert_eq!(
       result, example.expected_output,
--- 2015/rust/src/days/day05/day05_solver.rs
+++ 2015/rust/src/days/day05/day05_solver.rs
@@ -7,20 +7,49 @@ pub struct Day05Solver {
   pub year: i32,
 }
 
-impl SteppedSolver<Vec<i32>, Vec<i32>, i64, i64> for Day05Solver {
-  fn parse_input_one(&self, input: Vec<&str>) -> Vec<i32> {
-    unimplemented!()
+impl SteppedSolver<Vec<String>, Vec<String>, i64, i64> for Day05Solver {
+  fn parse_input_one(&self, input: Vec<&str>) -> Vec<String> {
+    input.iter().map(|x| x.to_string()).collect()
   }
 
-  fn parse_input_two(&self, input: Vec<&str>) -> Vec<i32> {
-    unimplemented!()
+  fn parse_input_two(&self, input: Vec<&str>) -> Vec<String> {
+    self.parse_input_one(input)
   }
 
-  fn solve_part_one(&self, input: Vec<i32>) -> i64 {
-    unimplemented!()
+  fn solve_part_one(&self, input: Vec<String>) -> i64 {
+    let count_nice = input.iter().filter(|x| is_nice(x)).count();
+    count_nice as i64
   }
 
-  fn solve_part_two(&self, input: Vec<i32>) -> i64 {
+  fn solve_part_two(&self, input: Vec<String>) -> i64 {
     unimplemented!()
   }
+}
+
+fn is_nice(x: &str) -> bool {
+  let mut vowels = 0;
+  let mut has_duplicate = false;
+  let mut last_char = ' ';
+
+  for c_char in x.chars() {
+    match c_char {
+      'a' | 'e' | 'i' | 'o' | 'u' => vowels += 1,
+      _ => (),
+    }
+
+    if c_char == last_char {
+      has_duplicate = true;
+    }
+
+    if (last_char == 'a' && c_char == 'b')
+      || (last_char == 'c' && c_char == 'd')
+      || (last_char == 'p' && c_char == 'q')
+      || (last_char == 'x' && c_char == 'y')
+    {
+      return false;
+    }
+
+    last_char = c_char;
+  }
+  vowels >= 3 && has_duplicate
 }
