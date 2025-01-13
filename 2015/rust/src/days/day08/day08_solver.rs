77e9663df51845f3e3711b17241abb6dcf8c2ac9
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-13 09:26:04 -0600

    Completed Part One

--- 2015/rust/src/days/day08/day08_solver.rs
+++ 2015/rust/src/days/day08/day08_solver.rs
@@ -7,20 +7,54 @@ pub struct Day08Solver {
   pub year: i32,
 }
 
-impl SteppedSolver<Vec<i32>, Vec<i32>, i64, i64> for Day08Solver {
-  fn parse_input_one(&self, input: Vec<&str>) -> Vec<i32> {
-    unimplemented!()
+impl SteppedSolver<Vec<String>, Vec<String>, i64, i64> for Day08Solver {
+  fn parse_input_one(&self, input: Vec<&str>) -> Vec<String> {
+    input.iter().map(|x| x.to_string()).collect()
   }
 
-  fn parse_input_two(&self, input: Vec<&str>) -> Vec<i32> {
-    unimplemented!()
+  fn parse_input_two(&self, input: Vec<&str>) -> Vec<String> {
+    self.parse_input_one(input)
   }
 
-  fn solve_part_one(&self, input: Vec<i32>) -> i64 {
-    unimplemented!()
+  fn solve_part_one(&self, input: Vec<String>) -> i64 {
+    let pairs = input.iter().map(|line| {
+      let mut code_sum = 0;
+      let mut memory_sum = 0;
+
+      let mut chars = line.chars();
+      while let Some(c) = chars.next() {
+        code_sum += 1;
+        if c == '"' {
+          continue;
+        }
+
+        if c == '\\' {
+          let next = chars.next().unwrap();
+          code_sum += 1;
+          if next == 'x' {
+            chars.next();
+            chars.next();
+            code_sum += 2;
+          }
+        }
+
+        memory_sum += 1;
+      }
+
+      (code_sum, memory_sum)
+    });
+
+    let mut total_code_sum = 0;
+    let mut total_memory_sum = 0;
+    for (code_sum, memory_sum) in pairs {
+      total_code_sum += code_sum;
+      total_memory_sum += memory_sum;
+    }
+
+    total_code_sum - total_memory_sum
   }
 
-  fn solve_part_two(&self, input: Vec<i32>) -> i64 {
+  fn solve_part_two(&self, input: Vec<String>) -> i64 {
     unimplemented!()
   }
 }
--- 2015/rust/src/days/day08/day08_tests.rs
+++ 2015/rust/src/days/day08/day08_tests.rs
@@ -8,8 +8,8 @@ const DAY_NUM: i32 = 8;
 
 #[test]
 fn sample_01_part_one() {
-  let raw_input = vec![];
-  let expected_output: i64 = 0;
+  let raw_input = vec!["\"\"", "\"abc\"", "\"aaa\\\"aaa\"", "\"\\x27\""];
+  let expected_output: i64 = 12;
 
   let solver = Day08Solver {
     day: DAY_NUM,
@@ -22,13 +22,12 @@ fn sample_01_part_one() {
 }
 
 #[test]
-#[ignore]
 fn solution_part_one() {
   let remote_input = PuzzleInputProvider::new_remote(YEAR_NUM, DAY_NUM)
     .read_input()
     .unwrap();
   let raw_input = remote_input.iter().map(|x| x.as_str()).collect();
-  const EXPECTED_SOLUTION_PART1: i64 = 0;
+  const EXPECTED_SOLUTION_PART1: i64 = 1350;
 
   let solver = Day08Solver {
     day: DAY_NUM,
