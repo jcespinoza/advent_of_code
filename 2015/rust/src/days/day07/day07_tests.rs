2a9ff1f90663e5a7b32b35f79f1347f5d69f681a
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-12 20:13:42 -0600

    Completed Part One

--- 2015/rust/src/days/day07/day07_tests.rs
+++ 2015/rust/src/days/day07/day07_tests.rs
@@ -8,8 +8,17 @@ const DAY_NUM: i32 = 7;
 
 #[test]
 fn sample_01_part_one() {
-  let raw_input = vec![];
-  let expected_output: i64 = 0;
+  let raw_input = vec![
+    "123 -> x",
+    "456 -> y",
+    "x AND y -> d",
+    "x OR y -> e",
+    "x LSHIFT 2 -> f",
+    "y RSHIFT 2 -> g",
+    "NOT x -> h",
+    "NOT y -> a",
+  ];
+  let expected_output: i64 = 65079;
 
   let solver = Day07Solver {
     day: DAY_NUM,
@@ -22,13 +31,12 @@ fn sample_01_part_one() {
 }
 
 #[test]
-#[ignore]
 fn solution_part_one() {
   let remote_input = PuzzleInputProvider::new_remote(YEAR_NUM, DAY_NUM)
     .read_input()
     .unwrap();
   let raw_input = remote_input.iter().map(|x| x.as_str()).collect();
-  const EXPECTED_SOLUTION_PART1: i64 = 0;
+  const EXPECTED_SOLUTION_PART1: i64 = 123;
 
   let solver = Day07Solver {
     day: DAY_NUM,
