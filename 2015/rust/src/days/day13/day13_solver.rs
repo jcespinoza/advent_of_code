73d57630141b8faf20afe876bb8b97e28913b2d8
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-15 11:21:42 -0600

    Completed Part Two

--- 2015/rust/src/days/day13/day13_solver.rs
+++ 2015/rust/src/days/day13/day13_solver.rs
@@ -24,50 +24,74 @@ impl SteppedSolver<Vec<SeatingCondition>, Vec<SeatingCondition>, i64, i64> for D
 
   fn solve_part_one(&self, input: Vec<SeatingCondition>) -> i64 {
     let all_guests: HashSet<String> = input.iter().map(|x| x.guest.clone()).collect();
-    let mut happiness_map: HashMap<(String, String), i32> = HashMap::new();
-    let mut max_happiness = 0;
+    let happiness_map = get_happiness_map(input, &all_guests);
 
-    for guest in all_guests.iter() {
-      for neighbor in all_guests.iter() {
-        if guest == neighbor {
-          continue;
-        }
-
-        let mut happiness = 0;
-        for condition in input.iter() {
-          if condition.guest == *guest && condition.neighbor == *neighbor {
-            match condition.change {
-              UnitChange::Gain(units) => happiness += units,
-              UnitChange::Lose(units) => happiness -= units,
-            }
-          }
-        }
-
-        happiness_map.insert((guest.clone(), neighbor.clone()), happiness);
-      }
-    }
-
-    for seating_arrangement in all_guests.iter().permutations(all_guests.len()) {
-      let mut happiness = 0;
-      for i in 0..seating_arrangement.len() {
-        let guest = &seating_arrangement[i];
-        let neighbor = &seating_arrangement[(i + 1) % seating_arrangement.len()];
-        happiness += happiness_map
-          .get(&(guest.to_string(), neighbor.to_string()))
-          .unwrap();
-        happiness += happiness_map
-          .get(&(neighbor.to_string(), guest.to_string()))
-          .unwrap();
-      }
-
-      if happiness > max_happiness {
-        max_happiness = happiness;
-      }
-    }
-    max_happiness as i64
+    compute_max_happiness(all_guests, happiness_map)
   }
 
   fn solve_part_two(&self, input: Vec<SeatingCondition>) -> i64 {
-    unimplemented!()
+    let mut all_guests: HashSet<String> = input.iter().map(|x| x.guest.clone()).collect();
+    all_guests.insert("Myself".to_string());
+    let happiness_map = get_happiness_map(input, &all_guests);
+
+    compute_max_happiness(all_guests, happiness_map)
   }
+}
+
+fn compute_max_happiness(
+  all_guests: HashSet<String>,
+  happiness_map: HashMap<(String, String), i32>,
+) -> i64 {
+  let mut max_happiness = 0;
+  for seating_arrangement in all_guests.iter().permutations(all_guests.len()) {
+    let mut happiness = 0;
+    for i in 0..seating_arrangement.len() {
+      let guest = &seating_arrangement[i];
+      let neighbor = &seating_arrangement[(i + 1) % seating_arrangement.len()];
+      happiness += match guest {
+        val if val.as_str() == "Myself" => &0,
+        _ => happiness_map
+          .get(&(guest.to_string(), neighbor.to_string()))
+          .unwrap(),
+      };
+      happiness += match neighbor {
+        val if val.as_str() == "Myself" => &0,
+        _ => happiness_map
+          .get(&(neighbor.to_string(), guest.to_string()))
+          .unwrap(),
+      };
+    }
+
+    if happiness > max_happiness {
+      max_happiness = happiness;
+    }
+  }
+  max_happiness as i64
+}
+
+fn get_happiness_map(
+  input: Vec<SeatingCondition>,
+  all_guests: &HashSet<String>,
+) -> HashMap<(String, String), i32> {
+  let mut happiness_map: HashMap<(String, String), i32> = HashMap::new();
+  for guest in all_guests.iter() {
+    for neighbor in all_guests.iter() {
+      if guest == neighbor {
+        continue;
+      }
+
+      let mut happiness = 0;
+      for condition in input.iter() {
+        if condition.guest == *guest && condition.neighbor == *neighbor {
+          match condition.change {
+            UnitChange::Gain(units) => happiness += units,
+            UnitChange::Lose(units) => happiness -= units,
+          }
+        }
+      }
+
+      happiness_map.insert((guest.clone(), neighbor.clone()), happiness);
+    }
+  }
+  happiness_map
 }
