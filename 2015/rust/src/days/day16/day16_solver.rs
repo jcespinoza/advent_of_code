99e9fd2ed5859da3eb48e5c1eb0c29baeb27fd13
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-16 08:21:23 -0600

    Completed Part One

--- 2015/rust/src/days/day16/day16_solver.rs
+++ 2015/rust/src/days/day16/day16_solver.rs
@@ -1,26 +1,41 @@
 #![allow(unused)]
 use crate::common::SteppedSolver;
 
+use super::{find_aunt, AuntSue, MachineResult};
+
 #[derive(Debug)]
 pub struct Day16Solver {
   pub day: i32,
   pub year: i32,
 }
 
-impl SteppedSolver<Vec<i32>, Vec<i32>, i64, i64> for Day16Solver {
-  fn parse_input_one(&self, input: Vec<&str>) -> Vec<i32> {
-    unimplemented!()
+impl SteppedSolver<Vec<AuntSue>, Vec<AuntSue>, i64, i64> for Day16Solver {
+  fn parse_input_one(&self, input: Vec<&str>) -> Vec<AuntSue> {
+    input.iter().map(|s| AuntSue::from(*s)).collect()
   }
 
-  fn parse_input_two(&self, input: Vec<&str>) -> Vec<i32> {
-    unimplemented!()
+  fn parse_input_two(&self, input: Vec<&str>) -> Vec<AuntSue> {
+    self.parse_input_one(input)
   }
 
-  fn solve_part_one(&self, input: Vec<i32>) -> i64 {
-    unimplemented!()
+  fn solve_part_one(&self, aunts: Vec<AuntSue>) -> i64 {
+    let machine_result = MachineResult {
+      children: 3,
+      cats: 7,
+      samoyeds: 2,
+      pomeranians: 3,
+      akitas: 0,
+      vizslas: 0,
+      goldfish: 5,
+      trees: 3,
+      cars: 2,
+      perfumes: 1,
+    };
+
+    find_aunt(&aunts, &machine_result)
   }
 
-  fn solve_part_two(&self, input: Vec<i32>) -> i64 {
+  fn solve_part_two(&self, aunts: Vec<AuntSue>) -> i64 {
     unimplemented!()
   }
 }
