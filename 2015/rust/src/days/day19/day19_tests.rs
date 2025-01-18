784ef79b24cb414a8f15d8fa9e62bd6fd58b20c1
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-18 11:34:24 -0600

    One more test

--- 2015/rust/src/days/day19/day19_tests.rs
+++ 2015/rust/src/days/day19/day19_tests.rs
@@ -9,6 +9,21 @@ const DAY_NUM: i32 = 19;
 #[test]
 fn sample_01_part_one() {
   let raw_input = vec!["H => HO", "H => OH", "O => HH", "", "HOH"];
+  let expected_output: i64 = 4;
+
+  let solver = Day19Solver {
+    day: DAY_NUM,
+    year: YEAR_NUM,
+  };
+  let input = solver.parse_input_one(raw_input);
+  let result = solver.solve_part_one(input);
+
+  assert_eq!(result, expected_output);
+}
+
+#[test]
+fn sample_02_part_one() {
+  let raw_input = vec!["H => HO", "H => OH", "O => HH", "", "HOHOHO"];
   let expected_output: i64 = 7;
 
   let solver = Day19Solver {
@@ -73,31 +88,6 @@ fn solution_part_two() {
   let result = solver.solve_part_two(input);
 
   assert_eq!(result, EXPECTED_SOLUTION_PART2);
-}
-
-#[test]
-#[ignore]
-fn examples_part_one() {
-  let examples: Vec<AocExample<i64>> = vec![AocExample {
-    raw_input: vec![],
-    expected_output: 0,
-  }];
-
-  let solver = Day19Solver {
-    day: DAY_NUM,
-    year: YEAR_NUM,
-  };
-
-  for (index, example) in examples.iter().enumerate() {
-    let input = solver.parse_input_one(example.raw_input.clone());
-    let result = solver.solve_part_one(input);
-
-    assert_eq!(
-      result, example.expected_output,
-      "Failed for Example: {:?}",
-      index
-    );
-  }
 }
 
 #[test]
