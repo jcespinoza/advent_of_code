0ebf56b0908b4d5628244839293146ebfac3d983
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-19 12:34:05 -0600

    Completed Part Two

--- 2015/rust/src/days/day20/day20_solver.rs
+++ 2015/rust/src/days/day20/day20_solver.rs
@@ -9,20 +9,21 @@ pub struct Day20Solver {
   pub year: i32,
 }
 
-impl SteppedSolver<i32, i32, i64, i64> for Day20Solver {
-  fn parse_input_one(&self, input: Vec<&str>) -> i32 {
+impl SteppedSolver<usize, usize, i64, i64> for Day20Solver {
+  fn parse_input_one(&self, input: Vec<&str>) -> usize {
     input.first().unwrap().parse().unwrap()
   }
 
-  fn parse_input_two(&self, input: Vec<&str>) -> i32 {
+  fn parse_input_two(&self, input: Vec<&str>) -> usize {
     self.parse_input_one(input)
   }
 
-  fn solve_part_one(&self, target_count: i32) -> i64 {
-    let mut house_present_count = vec![0; (target_count / 10) as usize];
+  fn solve_part_one(&self, target_present_count: usize) -> i64 {
+    let mut house_present_count = vec![0; (target_present_count / 10)];
 
     for elf_number in 1..house_present_count.len() {
       let mut house_number = elf_number;
+
       while house_number < house_present_count.len() {
         house_present_count[house_number] += elf_number * 10;
         house_number += elf_number;
@@ -31,13 +32,30 @@ impl SteppedSolver<i32, i32, i64, i64> for Day20Solver {
 
     let house_with_target_count = house_present_count
       .iter()
-      .position(|h| h >= &(target_count as usize))
+      .position(|h| h >= &target_present_count)
       .unwrap();
 
     house_with_target_count as i64
   }
 
-  fn solve_part_two(&self, input: i32) -> i64 {
-    unimplemented!()
+  fn solve_part_two(&self, target_present_count: usize) -> i64 {
+    let mut house_present_count = vec![0; (target_present_count / 10)];
+
+    for elf_number in 1..house_present_count.len() {
+      let mut house_number = elf_number;
+      let mut houses_visited = 0;
+      while house_number < house_present_count.len() && houses_visited < 50 {
+        house_present_count[house_number] += elf_number * 11;
+        house_number += elf_number;
+        houses_visited += 1;
+      }
+    }
+
+    let house_with_target_count = house_present_count
+      .iter()
+      .position(|h| h >= &target_present_count)
+      .unwrap();
+
+    house_with_target_count as i64
   }
 }
--- 2015/rust/src/days/day20/day20_tests.rs
+++ 2015/rust/src/days/day20/day20_tests.rs
@@ -7,22 +7,6 @@ use crate::{
 const DAY_NUM: i32 = 20;
 
 #[test]
-#[ignore]
-fn sample_01_part_one() {
-  let raw_input = vec!["130"];
-  let expected_output: i64 = 9;
-
-  let solver = Day20Solver {
-    day: DAY_NUM,
-    year: YEAR_NUM,
-  };
-  let input = solver.parse_input_one(raw_input);
-  let result = solver.solve_part_one(input);
-
-  assert_eq!(result, expected_output);
-}
-
-#[test]
 fn solution_part_one() {
   let remote_input = PuzzleInputProvider::new_remote(YEAR_NUM, DAY_NUM)
     .read_input()
@@ -41,29 +25,12 @@ fn solution_part_one() {
 }
 
 #[test]
-#[ignore]
-fn sample_01_part_two() {
-  let raw_input = vec![];
-  let expected_output: i64 = 0;
-
-  let solver = Day20Solver {
-    day: DAY_NUM,
-    year: YEAR_NUM,
-  };
-  let input = solver.parse_input_two(raw_input);
-  let result = solver.solve_part_two(input);
-
-  assert_eq!(result, expected_output);
-}
-
-#[test]
-#[ignore]
 fn solution_part_two() {
   let remote_input = PuzzleInputProvider::new_remote(YEAR_NUM, DAY_NUM)
     .read_input()
     .unwrap();
   let raw_input = remote_input.iter().map(|x| x.as_str()).collect();
-  const EXPECTED_SOLUTION_PART2: i64 = 0;
+  const EXPECTED_SOLUTION_PART2: i64 = 705600;
 
   let solver = Day20Solver {
     day: DAY_NUM,
@@ -111,6 +78,10 @@ fn examples_part_one() {
       raw_input: vec!["150"],
       expected_output: 8,
     },
+    AocExample {
+      raw_input: vec!["130"],
+      expected_output: 9,
+    },
   ];
 
   let solver = Day20Solver {
@@ -121,31 +92,6 @@ fn examples_part_one() {
   for (index, example) in examples.iter().enumerate() {
     let input = solver.parse_input_one(example.raw_input.clone());
     let result = solver.solve_part_one(input);
-
-    assert_eq!(
-      result, example.expected_output,
-      "Failed for Example: {:?}",
-      index
-    );
-  }
-}
-
-#[test]
-#[ignore]
-fn examples_part_two() {
-  let examples: Vec<AocExample<i64>> = vec![AocExample {
-    raw_input: vec![],
-    expected_output: 0,
-  }];
-
-  let solver = Day20Solver {
-    day: DAY_NUM,
-    year: YEAR_NUM,
-  };
-
-  for (index, example) in examples.iter().enumerate() {
-    let input = solver.parse_input_two(example.raw_input.clone());
-    let result = solver.solve_part_two(input);
 
     assert_eq!(
       result, example.expected_output,
