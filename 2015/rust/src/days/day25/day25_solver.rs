d2a9c8dc5e91a9bdfdda52306a047db55dd75a76
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-27 17:42:45 -0600

    Completed Part One

--- 2015/rust/src/days/day25/day25_solver.rs
+++ 2015/rust/src/days/day25/day25_solver.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
-use crate::common::SteppedSolver;
+use crate::{common::SteppedSolver, days::day03::Point};
+
+use super::compute_code_at;
 
 #[derive(Debug)]
 pub struct Day25Solver {
@@ -7,20 +9,39 @@ pub struct Day25Solver {
   pub year: i32,
 }
 
-impl SteppedSolver<Vec<i32>, Vec<i32>, i64, i64> for Day25Solver {
-  fn parse_input_one(&self, input: Vec<&str>) -> Vec<i32> {
-    unimplemented!()
+impl SteppedSolver<Point, Point, i64, i64> for Day25Solver {
+  fn parse_input_one(&self, input: Vec<&str>) -> Point {
+    let locations = input
+      .first()
+      .unwrap()
+      .split(". ")
+      .skip(1)
+      .map(|sentence| {
+        let parts = sentence.split(", ").collect::<Vec<&str>>();
+        let row = parts[0].split(" ").last().unwrap().parse::<i32>().unwrap();
+        let col = parts[1]
+          .split(" ")
+          .last()
+          .unwrap()
+          .trim_end_matches(".")
+          .parse::<i32>()
+          .unwrap();
+        Point { row, col }
+      })
+      .collect::<Vec<Point>>();
+
+    *locations.first().unwrap()
   }
 
-  fn parse_input_two(&self, input: Vec<&str>) -> Vec<i32> {
-    unimplemented!()
+  fn parse_input_two(&self, input: Vec<&str>) -> Point {
+    self.parse_input_one(input)
   }
 
-  fn solve_part_one(&self, input: Vec<i32>) -> i64 {
-    unimplemented!()
+  fn solve_part_one(&self, target_location: Point) -> i64 {
+    compute_code_at(target_location)
   }
 
-  fn solve_part_two(&self, input: Vec<i32>) -> i64 {
+  fn solve_part_two(&self, target_location: Point) -> i64 {
     unimplemented!()
   }
 }
--- 2015/rust/src/days/day25/day25_tests.rs
+++ 2015/rust/src/days/day25/day25_tests.rs
@@ -8,8 +8,10 @@ const DAY_NUM: i32 = 25;
 
 #[test]
 fn sample_01_part_one() {
-  let raw_input = vec![];
-  let expected_output: i64 = 0;
+  let raw_input = vec![
+    "To continue, please consult the code grid in the manual.  Enter the code at row 2, column 1.",
+  ];
+  let expected_output: i64 = 31916031;
 
   let solver = Day25Solver {
     day: DAY_NUM,
@@ -22,13 +24,12 @@ fn sample_01_part_one() {
 }
 
 #[test]
-#[ignore]
 fn solution_part_one() {
   let remote_input = PuzzleInputProvider::new_remote(YEAR_NUM, DAY_NUM)
     .read_input()
     .unwrap();
   let raw_input = remote_input.iter().map(|x| x.as_str()).collect();
-  const EXPECTED_SOLUTION_PART1: i64 = 0;
+  const EXPECTED_SOLUTION_PART1: i64 = 19980801;
 
   let solver = Day25Solver {
     day: DAY_NUM,
@@ -76,11 +77,23 @@ fn solution_part_two() {
 }
 
 #[test]
-#[ignore]
 fn examples_part_one() {
   let examples: Vec<AocExample<i64>> = vec![AocExample {
-    raw_input: vec![],
-    expected_output: 0,
+    raw_input: vec!["To continue, please consult the code grid in the manual.  Enter the code at row 3, column 4."],
+    expected_output: 7981243,
+  },AocExample {
+    raw_input: vec!["To continue, please consult the code grid in the manual.  Enter the code at row 6, column 1."],
+    expected_output: 33071741,
+  },AocExample {
+    raw_input: vec!["To continue, please consult the code grid in the manual.  Enter the code at row 2, column 3."],
+    expected_output: 16929656,
+  },AocExample {
+    raw_input: vec!["To continue, please consult the code grid in the manual.  Enter the code at row 5, column 6."],
+    expected_output: 31663883,
+  },
+  AocExample {
+    raw_input: vec!["To continue, please consult the code grid in the manual.  Enter the code at row 4, column 3."],
+    expected_output: 21345942,
   }];
 
   let solver = Day25Solver {
