2ce19295af1ab9d23099a6c4618cff869b9c9aa8
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-01-27 15:57:40 -0600

    Adding 2015 Day 25 in Rust

---
+++ 2015/rust/src/days/day25/day25_impl.rs
@@ -0,0 +1,28 @@
+// region: defaul_implementations
+use crate::{
+  common::{HasDayYear, SolverRunner, SteppedSolver},
+  days::day25::day25_solver::Day25Solver,
+};
+
+impl HasDayYear for Day25Solver {
+  fn day(&self) -> i32 {
+    self.day
+  }
+
+  fn year(&self) -> i32 {
+    self.year
+  }
+}
+
+impl SolverRunner for Day25Solver {
+  fn solve_part_one_str(&self, input: Vec<&str>) -> String {
+    let parsed_input = self.parse_input_one(input);
+    self.solve_part_one(parsed_input).to_string()
+  }
+
+  fn solve_part_two_str(&self, input: Vec<&str>) -> String {
+    let parsed_input = self.parse_input_two(input);
+    self.solve_part_two(parsed_input).to_string()
+  }
+}
+// endregion
