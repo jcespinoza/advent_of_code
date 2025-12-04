12bd1f5b7467aa2cf9da9ddb5df732f773e88a4a
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-12-04 17:02:18 -0600

    Adding 2025 Day 03 in C#

---
+++ 2025/csharp/Advent.Days/Day03/Day03Solver.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Commons;
+
+namespace Advent.Days
+{
+    public class Day03Solver : Solver<object[], long>
+    {
+        public Day03Solver() : base(2025, 03) { }
+
+        public override object[] ParseInput(IEnumerable<string> input)
+            => input.ToArray();
+
+
+        public override long PartOne(object[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long PartTwo(object[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
