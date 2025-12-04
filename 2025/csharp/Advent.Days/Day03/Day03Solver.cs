19f3f95f3a9ad26725f8a7ed88984bee2265f85e
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-12-04 17:23:02 -0600

    Adding 2025 Day 03 in C# (#88)
    * Adding 2025 Day 03 in C#
    
    * Completed Part One and Two by porting the rust solution

---
+++ 2025/csharp/Advent.Days/Day03/Day03Solver.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.Commons;
+
+namespace Advent.Days
+{
+    public class Day03Solver : Solver<BatteryBank[], long>
+    {
+        public Day03Solver() : base(2025, 03) { }
+
+        public override BatteryBank[] ParseInput(IEnumerable<string> input)
+            => input.Select(BatteryBank.From).ToArray();
+
+        public override long PartOne(BatteryBank[] input)
+        {
+            var numberOfBatteries = 2;
+            long totalCharge = 0;
+
+            foreach (var bank in input)
+            {
+                var top = bank.TopNBatteries(numberOfBatteries);
+                var chargeStr = string.Concat(top.Select(d => d.ToString()));
+                var charge = long.Parse(chargeStr);
+                totalCharge += charge;
+            }
+
+            return totalCharge;
+        }
+
+        public override long PartTwo(BatteryBank[] input)
+        {
+            var numberOfBatteries = 12;
+            long totalCharge = 0;
+
+            foreach (var bank in input)
+            {
+                var top = bank.TopNBatteries(numberOfBatteries);
+                var chargeStr = string.Concat(top.Select(d => d.ToString()));
+                var charge = long.Parse(chargeStr);
+                totalCharge += charge;
+            }
+
+            return totalCharge;
+        }
+    }
+}
---
+++ 2025/csharp/Advent.Days/Day03/Day03Test.cs
@@ -0,0 +1,84 @@
+﻿using AdventOfCode.Commons;
+using dotenv.net;
+
+namespace Advent.Days
+{
+    public class Day03Test : TestEngine<Day03Solver, BatteryBank[], long>
+    {
+        private const long EXPECTED_SOLUTION_PART_1 = 17155;
+        private const long EXPECTED_SOLUTION_PART_2 = 169685670469164;
+
+        public Day03Test()
+        {
+            DotEnv.Load();
+        }
+
+        public override Puzzle PartOne => new()
+        {
+            ShouldSkipTests = false,
+            Example = new()
+            {
+                RawInput = [
+                    "987654321111111",
+                    "811111111111119",
+                    "234234234234278",
+                    "818181911112111",
+                ],
+                Result = 357,
+            },
+            Examples = [
+                new () {
+                    RawInput = ["987654321111111"],
+                    Result = 98
+                },
+                new () {
+                    RawInput = ["811111111111119"],
+                    Result = 89
+                },
+                new () {
+                    RawInput = ["234234234234278"],
+                    Result = 78
+                },
+                new () {
+                    RawInput = ["818181911112111"],
+                    Result = 92
+                }
+            ],
+            Solution = EXPECTED_SOLUTION_PART_1,
+        };
+
+        public override Puzzle PartTwo => new()
+        {
+            ShouldSkipTests = false,
+            Example = new()
+            {
+                RawInput = [
+                    "987654321111111",
+                    "811111111111119",
+                    "234234234234278",
+                    "818181911112111",
+                ],
+                Result = 3121910778619,
+            },
+            Examples = [
+                new () {
+                    RawInput = ["987654321111111"],
+                    Result = 987654321111
+                },
+                new () {
+                    RawInput = ["811111111111119"],
+                    Result = 811111111119
+                },
+                new () {
+                    RawInput = ["234234234234278"],
+                    Result = 434234234278
+                },
+                new () {
+                    RawInput = ["818181911112111"],
+                    Result = 888911112111
+                }
+            ],
+            Solution = EXPECTED_SOLUTION_PART_2,
+        };
+    }
+}
