fd200d8e27d20b7a051defece8640082cc217e93
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-12-04 12:53:23 -0600

    Adding 2025 Day 04 in Rust

--- 2025/rust/src/integration/solver_mapping.rs
+++ 2025/rust/src/integration/solver_mapping.rs
@@ -4,6 +4,7 @@ use crate::{
   days::day01::Day01Solver,
   days::day02::Day02Solver,
   days::day03::Day03Solver,
+  days::day04::Day04Solver,
   //NEXT_IMPORT
 };
 
@@ -12,6 +13,7 @@ pub enum DayNum {
   Day01 = 1,
   Day02 = 2,
   Day03 = 3,
+  Day04 = 4,
   //NEXT_ENUM_ENTRY
 }
 
@@ -23,6 +25,7 @@ impl TryFrom<i32> for DayNum {
       x if x == DayNum::Day01 as i32 => Ok(DayNum::Day01),
       x if x == DayNum::Day02 as i32 => Ok(DayNum::Day02),
       x if x == DayNum::Day03 as i32 => Ok(DayNum::Day03),
+      x if x == DayNum::Day04 as i32 => Ok(DayNum::Day04),
       //NEXT_ENUM_TRY_FROM
       _ => Err(()),
     }
@@ -38,6 +41,7 @@ pub fn create_solver(year: i32, day: i32) -> Box<dyn SolverRunner> {
     DayNum::Day01 => Box::new(Day01Solver { day: 1, year }),
     DayNum::Day02 => Box::new(Day02Solver { day: 2, year }),
     DayNum::Day03 => Box::new(Day03Solver { day: 3, year }),
+    DayNum::Day04 => Box::new(Day04Solver { day: 4, year }),
     //NEXT_ENUM_MATCH
   }
 }
