4ecb7a6fde827b799bb4403bfd97afae5b3b9d56
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-12-07 13:28:35 -0600

    Adding 2025 Day 05 in Rust (#90)
    * Adding 2025 Day 05 in Rust
    
    * Completed Part One by checking in a simple loop
    
    * Completed Part Two by merging ranges

---
+++ 2025/rust/src/days/day05/day05_solver.rs
@@ -0,0 +1,48 @@
+#![allow(unused)]
+use crate::{
+  common::SteppedSolver,
+  days::day05::{merge_ranges, IngrendientDb},
+};
+
+#[derive(Debug)]
+pub struct Day05Solver {
+  pub day: i32,
+  pub year: i32,
+}
+
+impl SteppedSolver<IngrendientDb, IngrendientDb, i64, i64> for Day05Solver {
+  fn parse_input_one(&self, input: Vec<&str>) -> IngrendientDb {
+    IngrendientDb::from(input)
+  }
+
+  fn parse_input_two(&self, input: Vec<&str>) -> IngrendientDb {
+    self.parse_input_one(input)
+  }
+
+  fn solve_part_one(&self, input: IngrendientDb) -> i64 {
+    let mut fresh_count: i64 = 0;
+
+    // Iterate ver the available ingredient IDs
+    for &id in &input.available_ids {
+      // Check if the ID falls within any of the fresh ID ranges
+      let is_fresh = input
+        .fresh_id_ranges
+        .iter()
+        .any(|&(start, end)| id >= start && id <= end);
+      if is_fresh {
+        fresh_count += 1;
+      }
+    }
+    fresh_count
+  }
+
+  fn solve_part_two(&self, input: IngrendientDb) -> i64 {
+    let merged_ranges = merge_ranges(&input.fresh_id_ranges);
+
+    // Now sum up the total number of fresh IDs in the merged ranges
+    merged_ranges
+      .iter()
+      .map(|&(start, end)| end - start + 1)
+      .sum()
+  }
+}
---
+++ 2025/rust/src/days/day05/day05_tests.rs
@@ -0,0 +1,78 @@
+#![allow(unused)]
+use crate::{
+  common::{AocExample, PuzzleInputProvider, SteppedSolver, YEAR_NUM},
+  days::day05::Day05Solver,
+};
+
+const DAY_NUM: i32 = 5;
+
+#[test]
+fn sample_01_part_one() {
+  let raw_input = vec![
+    "3-5", "10-14", "16-20", "12-18", "", "1", "5", "8", "11", "17", "32",
+  ];
+  let expected_output: i64 = 3;
+
+  let solver = Day05Solver {
+    day: DAY_NUM,
+    year: YEAR_NUM,
+  };
+  let input = solver.parse_input_one(raw_input);
+  let result = solver.solve_part_one(input);
+
+  assert_eq!(result, expected_output);
+}
+
+#[test]
+fn solution_part_one() {
+  #[allow(dead_code)]
+  let remote_input = PuzzleInputProvider::new_remote(YEAR_NUM, DAY_NUM)
+    .read_input()
+    .unwrap();
+  let raw_input = remote_input.iter().map(|x| x.as_str()).collect();
+  const EXPECTED_SOLUTION_PART1: i64 = 638;
+
+  let solver = Day05Solver {
+    day: DAY_NUM,
+    year: YEAR_NUM,
+  };
+  let input = solver.parse_input_one(raw_input);
+  let result = solver.solve_part_one(input);
+
+  assert_eq!(result, EXPECTED_SOLUTION_PART1);
+}
+
+#[test]
+fn sample_01_part_two() {
+  let raw_input = vec![
+    "3-5", "10-14", "16-20", "12-18", "", "1", "5", "8", "11", "17", "32",
+  ];
+  let expected_output: i64 = 14;
+
+  let solver = Day05Solver {
+    day: DAY_NUM,
+    year: YEAR_NUM,
+  };
+  let input = solver.parse_input_two(raw_input);
+  let result = solver.solve_part_two(input);
+
+  assert_eq!(result, expected_output);
+}
+
+#[test]
+fn solution_part_two() {
+  let remote_input = PuzzleInputProvider::new_remote(YEAR_NUM, DAY_NUM)
+    .read_input()
+    .unwrap();
+  let raw_input = remote_input.iter().map(|x| x.as_str()).collect();
+  const EXPECTED_SOLUTION_PART2: i64 = 352946349407338;
+
+  let solver = Day05Solver {
+    day: DAY_NUM,
+    year: YEAR_NUM,
+  };
+  let input = solver.parse_input_two(raw_input);
+  let result = solver.solve_part_two(input);
+
+  assert_eq!(result, EXPECTED_SOLUTION_PART2);
+}
