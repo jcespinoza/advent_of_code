c65a551243b1b8fa79208a2eb1908dc8b019717d
Juan Carlos Espinoza <dev@jcespinoza.com>
2025-12-08 18:24:34 -0600

    Adding 2025 Day 08 in Rust

---
+++ 2025/rust/src/days/day08/day08_solver.rs
@@ -0,0 +1,26 @@
+#![allow(unused)]
+use crate::common::SteppedSolver;
+
+#[derive(Debug)]
+pub struct Day08Solver {
+  pub day: i32,
+  pub year: i32,
+}
+
+impl SteppedSolver<Vec<i32>, Vec<i32>, i64, i64> for Day08Solver {
+  fn parse_input_one(&self, input: Vec<&str>) -> Vec<i32> {
+    unimplemented!()
+  }
+
+  fn parse_input_two(&self, input: Vec<&str>) -> Vec<i32> {
+    unimplemented!()
+  }
+
+  fn solve_part_one(&self, input: Vec<i32>) -> i64 {
+    unimplemented!()
+  }
+
+  fn solve_part_two(&self, input: Vec<i32>) -> i64 {
+    unimplemented!()
+  }
+}
